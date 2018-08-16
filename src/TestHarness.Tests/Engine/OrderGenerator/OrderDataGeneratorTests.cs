130d2912213c783e5c9fc4b6942419146005be07
Ryan Trenchard <wintermuteai@outlook.com>
2018-08-16 16:22:13 +0100

    RDPB-2607 added functionality for a data order generator pending a trade stream being developed

---
+++ src/TestHarness.Tests/Engine/OrderGenerator/OrderDataGeneratorTests.cs
@@ -0,0 +1,141 @@
+﻿using Domain.Equity.Trading;
+using FakeItEasy;
+using NLog;
+using NUnit.Framework;
+using System;
+using TestHarness.Engine.OrderGenerator;
+
+namespace TestHarness.Tests.Engine.OrderGenerator
+{
+    [TestFixture]
+    public class OrderDataGeneratorTests
+    {
+        private IDisposable _unsubscriber;
+        private IStockExchangeStream _stockStream;
+        private ILogger _logger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _unsubscriber = A.Fake<IDisposable>();
+            _stockStream = A.Fake<IStockExchangeStream>();
+            _logger = A.Fake<ILogger>();
+
+            A
+                .CallTo(() => _stockStream.Subscribe(A<IObserver<ExchangeTick>>.Ignored))
+                .Returns(_unsubscriber);
+        }
+
+        [Test]
+        public void Constructor_ConsidersNullLogger_ToBeExceptional()
+        {
+            Assert.Throws<ArgumentNullException>(() => new OrderDataGenerator(null));
+        }
+
+        [Test]
+        public void InitiateTrading_WithNullStockStream_LogsError()
+        {
+            var orderDataGenerator = new OrderDataGenerator(_logger);
+
+            orderDataGenerator.InitiateTrading(null);
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Error, "Initiation attempt in order data generator with null stock stream"))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void InitiateTrading_LogsInitiation()
+        {
+            var orderDataGenerator = new OrderDataGenerator(_logger);
+
+            orderDataGenerator.InitiateTrading(_stockStream);
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Info, "Order data generator initiated with new stock stream"))
+                .MustHaveHappenedOnceExactly();
+
+            A
+                .CallTo(() => _stockStream.Subscribe(A<IObserver<ExchangeTick>>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void InitiateTrading_WithExistingTrading_DoesNotDeadLockOnTerminatingOldTrading()
+        {
+            var orderDataGenerator = new OrderDataGenerator(_logger);
+
+            orderDataGenerator.InitiateTrading(_stockStream);
+            orderDataGenerator.InitiateTrading(_stockStream);
+
+            A
+                .CallTo(() => _unsubscriber.Dispose())
+                .MustHaveHappenedOnceExactly();
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Warn, "Initiating new trading with predecessor active"))
+                .MustHaveHappenedOnceExactly();
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Info, "Order data generator initiated with new stock stream"))
+                .MustHaveHappenedTwiceExactly();
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Info, "Order data generator terminating trading"))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void TerminateTrading_LogsOnTermination()
+        {
+            var orderDataGenerator = new OrderDataGenerator(_logger);
+
+            orderDataGenerator.TerminateTrading();
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Info, "Order data generator terminating trading"))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void OnCompleted_LogsMessageAndCallsTerminate()
+        {
+            var orderDataGenerator = new OrderDataGenerator(_logger);
+
+            orderDataGenerator.OnCompleted();
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Info, "Order data generator received completed message from stock stream. Terminating order data generation"))
+                .MustHaveHappenedOnceExactly();
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Info, "Order data generator terminating trading"))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void OnError_LogsWhenException_IsNull()
+        {
+            var orderDataGenerator = new OrderDataGenerator(_logger);
+
+            orderDataGenerator.OnError(null);
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Error, "Order data generator receieved a null exception OnError"))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void OnError_LogsWhenException_IsPassedIn()
+        {
+            var orderDataGenerator = new OrderDataGenerator(_logger);
+            var exception = new StackOverflowException();
+
+            orderDataGenerator.OnError(exception);
+
+            A
+                .CallTo(() => _logger.Log(LogLevel.Error, A<Exception>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+    }
+}
---
+++ src/TestHarness/Engine/OrderGenerator/OrderDataGenerator.cs
@@ -0,0 +1,94 @@
+﻿using Domain.Equity.Trading;
+using NLog;
+using System;
+
+namespace TestHarness.Engine.OrderGenerator
+{
+    public class OrderDataGenerator : IObserver<ExchangeTick>
+    {
+        private IDisposable _unsubscriber;
+        private IStockExchangeStream _stockStream;
+
+        private object _stateTransition = new object();
+        private volatile bool _generatorExecuting;
+
+        private ILogger _logger;
+
+        public OrderDataGenerator(ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public void InitiateTrading(IStockExchangeStream stockStream)
+        {
+            lock (_stateTransition)
+            {
+                if (stockStream == null)
+                {
+                    _logger.Log(LogLevel.Error, "Initiation attempt in order data generator with null stock stream");
+                    return;
+                }
+
+                if (_generatorExecuting)
+                {
+                    _logger.Log(LogLevel.Warn, "Initiating new trading with predecessor active");
+                    _TerminateTrading();
+                }
+
+                _logger.Log(LogLevel.Info, "Order data generator initiated with new stock stream");
+                _stockStream = stockStream;
+                _unsubscriber = stockStream.Subscribe(this);
+                _generatorExecuting = true;
+            }
+        }
+
+        public void OnCompleted()
+        {
+            _logger.Log(LogLevel.Info, "Order data generator received completed message from stock stream. Terminating order data generation");
+            this.TerminateTrading();
+        }
+
+        public void OnError(Exception error)
+        {
+            if (error == null)
+            {
+                _logger.Log(LogLevel.Error, "Order data generator receieved a null exception OnError");
+                return;
+            }
+
+            _logger.Log(LogLevel.Error, error);
+        }
+
+        public void OnNext(ExchangeTick value)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Avoid calling this from inside another state transition
+        /// </summary>
+        public void TerminateTrading()
+        {
+            lock (_stateTransition)
+            {
+                _TerminateTrading();
+            }
+        }
+
+        /// <summary>
+        /// Avoid dead locks with initiation terminating old trades
+        /// </summary>
+        private void _TerminateTrading()
+        {
+            _logger.Log(LogLevel.Info, "Order data generator terminating trading");
+
+            if (_unsubscriber != null)
+            {
+                _unsubscriber.Dispose();
+            }
+
+            _stockStream = null;
+            _generatorExecuting = false;
+        }
+    }
+}

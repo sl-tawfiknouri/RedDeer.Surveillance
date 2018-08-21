ec3bcf69b984e216322a28c0f4bd0c8b2e695be6
Ryan Trenchard <wintermuteai@outlook.com>
2018-08-21 10:31:59 +0100

    RDPB-2667 Added a surveillance .net standard library to hold the implementation logic for the surveillance app

---
+++ src/Surveillance/Surveillance/Mediator.cs
@@ -0,0 +1,9 @@
+﻿namespace Surveillance
+{
+    /// <summary>
+    /// The mediator orchestrates program components; factory; services; display
+    /// </summary>
+    public class Mediator
+    {
+    }
+}
---
+++ src/Surveillance/Surveillance/SurveillanceRegistry.cs
@@ -0,0 +1,23 @@
+﻿using StructureMap;
+using NLog.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using Domain.Equity.Trading.Streams.Interfaces;
+using Domain.Equity.Trading;
+using Utilities.Websockets;
+
+namespace Surveillance
+{
+    public class SurveillanceRegistry : Registry
+    {
+        public SurveillanceRegistry()
+        {
+            var loggerFactory = new NLogLoggerFactory();
+            For(typeof(ILoggerFactory)).Use(loggerFactory);
+            For(typeof(ILogger<>)).Use(typeof(Logger<>));
+
+
+            For<IWebsocketFactory>().Use<WebsocketFactory>();
+            For(typeof(IUnsubscriberFactory<>)).Use(typeof(UnsubscriberFactory<>));
+        }
+    }
+}

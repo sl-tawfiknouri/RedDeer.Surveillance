31124417f431dc0064f0b38ab57522823337ba6a
Ryan Trenchard <wintermuteai@outlook.com>
2018-08-23 15:56:12 +0100

    RDPB-2667 Added network failover local memory concept

---
+++ src/Utilities/Network IO/Websocket Connections/Interfaces/INetworkFailover.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Utilities.Network_IO.Websocket_Connections.Interfaces
+{
+    public interface INetworkFailover<T>
+    {
+        void Store(T value);
+        IReadOnlyCollection<T> Retrieve();
+        IReadOnlyCollection<T> RetrieveAndRemove();
+    }
+}
---
+++ src/Utilities/Network IO/Websocket Connections/NetworkFailoverLocalMemory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Utilities.Network_IO.Websocket_Connections.Interfaces;
+
+namespace Utilities.Network_IO.Websocket_Connections
+{
+    public class NetworkFailoverLocalMemory<T> : INetworkFailover<T>
+    {
+        private List<T> _storedData;
+        private object _lock = new object();
+
+        public NetworkFailoverLocalMemory()
+        {
+            _storedData = new List<T>();
+        }
+
+        public void Store(T value)
+        {
+            lock (_lock)
+            {
+                if (value == null)
+                {
+                    return;
+                }
+
+                _storedData.Add(value);
+            }
+        }
+
+        public IReadOnlyCollection<T> Retrieve()
+        {
+            lock (_lock)
+            {
+                return _storedData;
+            }
+        }
+
+        public IReadOnlyCollection<T> RetrieveAndRemove()
+        {
+            lock (_lock)
+            {
+                var newStoredData = new List<T>(_storedData);
+                _storedData = new List<T>();
+
+                return newStoredData;
+            }
+        }
+    }
+}

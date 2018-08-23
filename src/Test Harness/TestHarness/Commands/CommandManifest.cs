2930408a46a53d446a47ab5fc4c578777c617dbf
Ryan Trenchard <wintermuteai@outlook.com>
2018-08-23 15:26:51 +0100

    RDPB-2667 updated command manifest with recent commands

--- src/Test Harness/TestHarness/Commands/CommandManifest.cs
+++ src/Test Harness/TestHarness/Commands/CommandManifest.cs
@@ -7,5 +7,10 @@ namespace TestHarness.Commands
         public string Initiate { get { return "initiate"; } }
         public string Quit { get { return "quit"; } }
         public string Help { get { return "help"; } }
+        public string RunDemo { get { return "run demo"; } }
+        public string StopDemo { get { return "stop demo"; } }
+        public string RunDemoWithNetworking { get { return "run demo networking"; } }
+        public string StopDemoWithNetworking { get { return "stop demo networking"; } }
+        public string BuyProhibitedSecurity { get { return "buy prohibited security"; } }
     }
 }
--- src/Test Harness/TestHarness/Commands/Interfaces/ICommandManifest.cs
+++ src/Test Harness/TestHarness/Commands/Interfaces/ICommandManifest.cs
@@ -5,5 +5,10 @@
         string Help { get; }
         string Initiate { get; }
         string Quit { get; }
+        string RunDemo { get; }
+        string StopDemo { get; }
+        string RunDemoWithNetworking { get; }
+        string StopDemoWithNetworking { get; }
+        string BuyProhibitedSecurity { get; }
     }
 }

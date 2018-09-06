6686e6ed5ad86f3229ce019d5bf29d1e33cb6e00
Ryan Trenchard <wintermuteai@outlook.com>
2018-09-06 17:27:49 +0100

    RDPB-2736 Added quit command tests

---
+++ src/Test Harness/TestHarness.Tests/Commands/QuitCommandTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using TestHarness.Commands;
+using TestHarness.State;
+
+namespace TestHarness.Tests.Commands
+{
+    [TestFixture]
+    public class QuitCommandTests
+    {
+        [Test]
+        public void Constructor_NullProgramState_Exceptional()
+        {
+            Assert.Throws<ArgumentNullException>(() => new QuitCommand(null));
+        }
+
+        [Test]
+        public void Run_SetsProgramStateExecution_ToFalse()
+        {
+            var state = new ProgramState {Executing = true};
+            var command = new QuitCommand(state);
+
+            command.Run(string.Empty);
+
+            Assert.IsFalse(state.Executing);
+        }
+
+        [Test]
+        public void Handles_DoesNotHandle_Null()
+        {
+            var state = new ProgramState { Executing = true };
+            var command = new QuitCommand(state);
+
+            var result = command.Handles(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Handles_HandlesQuitCommand_LowerCase()
+        {
+            var state = new ProgramState { Executing = true };
+            var command = new QuitCommand(state);
+
+            var result = command.Handles("quit");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Handles_HandlesquitCommand_UpperCase()
+        {
+            var state = new ProgramState { Executing = true };
+            var command = new QuitCommand(state);
+
+            var result = command.Handles("QUIT");
+
+            Assert.IsTrue(result);
+        }
+    }
+}

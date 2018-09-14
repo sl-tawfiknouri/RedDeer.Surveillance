268b0da56f41f2a15051a8117cab0d86953148f9
Ryan Trenchard <wintermuteai@outlook.com>
2018-09-14 09:04:17 +0100

    RDPB-2736 updated surveillance start up to work for running as a service

--- src/Surveillance/App/Startup.cs
+++ src/Surveillance/App/Startup.cs
@@ -2,10 +2,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StructureMap;
 using Surveillance;
+using Surveillance.Configuration;
+using Surveillance.Configuration.Interfaces;
 using Surveillance.DataLayer;
+using Surveillance.DataLayer.Configuration;
+using Surveillance.DataLayer.Configuration.Interfaces;
+using Utilities.Aws_IO.Interfaces;
 
 namespace RedDeer.Surveillance.App
 {
@@ -16,6 +22,16 @@ namespace RedDeer.Surveillance.App
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             var container = new Container();
+            
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var dbConfiguration = BuildDatabaseConfiguration(configurationBuilder);
+            container.Inject(typeof(INetworkConfiguration), BuildNetworkConfiguration(configurationBuilder));
+            container.Inject(typeof(IElasticSearchConfiguration), dbConfiguration);
+            container.Inject(typeof(IAwsConfiguration), dbConfiguration);
+
             container.Configure(config =>
             {
                 config.IncludeRegistry<DataLayerRegistry>();
@@ -23,6 +39,7 @@ namespace RedDeer.Surveillance.App
                 config.IncludeRegistry<AppRegistry>();
                 config.Populate(services);
             });
+
 
             return container.GetInstance<IServiceProvider>();
         }
@@ -39,6 +56,36 @@ namespace RedDeer.Surveillance.App
             {
                 await context.Response.WriteAsync("Red Deer Surveillance Service App");
             });
+        }
+
+        private static INetworkConfiguration BuildNetworkConfiguration(IConfigurationRoot configurationBuilder)
+        {
+            var networkConfiguration = new NetworkConfiguration
+            {
+                SurveillanceServiceEquityDomain = configurationBuilder.GetValue<string>("SurveillanceServiceEquityDomain"),
+                SurveillanceServiceEquityPort = configurationBuilder.GetValue<string>("SurveillanceServiceEquityPort"),
+
+                SurveillanceServiceTradeDomain = configurationBuilder.GetValue<string>("SurveillanceServiceTradeDomain"),
+                SurveillanceServiceTradePort = configurationBuilder.GetValue<string>("SurveillanceServiceTradePort"),
+            };
+
+            return networkConfiguration;
+        }
+
+        private static IElasticSearchConfiguration BuildDatabaseConfiguration(IConfigurationRoot configurationBuilder)
+        {
+            var networkConfiguration = new ElasticSearchConfiguration
+            {
+                IsEc2Instance = configurationBuilder.GetValue<bool?>("IsEc2Instance") ?? false,
+                AwsSecretKey = configurationBuilder.GetValue<string>("AwsSecretKey"),
+                AwsAccessKey = configurationBuilder.GetValue<string>("AwsAccessKey"),
+                ScheduledRuleQueueName = configurationBuilder.GetValue<string>("ScheduledRuleQueueName"),
+                ElasticSearchProtocol = configurationBuilder.GetValue<string>("ElasticSearchProtocol"),
+                ElasticSearchDomain = configurationBuilder.GetValue<string>("ElasticSearchDomain"),
+                ElasticSearchPort = configurationBuilder.GetValue<string>("ElasticSearchPort")
+            };
+
+            return networkConfiguration;
         }
     }
 }

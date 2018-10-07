031a8abcd64e0652fb26e56a472ae1b6925ef3b5
Ryan Trenchard <wintermuteai@outlook.com>
2018-10-07 10:51:57 +0100

    RDPB-2936 added dynamo db to surveillance I think...cant test atm

--- src/Surveillance/App/Configuration/Configuration.cs
+++ src/Surveillance/App/Configuration/Configuration.cs
@@ -1,66 +1,175 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Microsoft.Extensions.Configuration;
 using Surveillance.Configuration;
 using Surveillance.Configuration.Interfaces;
 using Surveillance.DataLayer.Configuration;
 using Surveillance.DataLayer.Configuration.Interfaces;
 using Surveillance.System.DataLayer;
 using Surveillance.System.DataLayer.Interfaces;
+// ReSharper disable InconsistentlySynchronizedField
 
 namespace RedDeer.Surveillance.App.Configuration
 {
     public class Configuration
     {
+        private const string DynamoDbKey = "EnvironmentClientDeployment";
+        private const string DynamoDbTable = "surveillance-application";
+
+        private IDictionary<string, string> _dynamoConfig;
+        private bool _hasFetchedEc2Data;
+        private readonly object _lock = new object();
+
+        public Configuration()
+        {
+            _dynamoConfig = new Dictionary<string, string>();
+        }
+
         public INetworkConfiguration BuildNetworkConfiguration(IConfigurationRoot configurationBuilder)
         {
-            var networkConfiguration = new NetworkConfiguration
+            lock (_lock)
             {
-                SurveillanceServiceEquityDomain = configurationBuilder.GetValue<string>("SurveillanceServiceEquityDomain"),
-                SurveillanceServiceEquityPort = configurationBuilder.GetValue<string>("SurveillanceServiceEquityPort"),
+                Ec2Check(configurationBuilder);
 
-                SurveillanceServiceTradeDomain = configurationBuilder.GetValue<string>("SurveillanceServiceTradeDomain"),
-                SurveillanceServiceTradePort = configurationBuilder.GetValue<string>("SurveillanceServiceTradePort"),
-            };
+                var networkConfiguration = new NetworkConfiguration
+                {
+                    SurveillanceServiceEquityDomain = GetValue("SurveillanceServiceEquityDomain", configurationBuilder),
+                    SurveillanceServiceEquityPort = GetValue("SurveillanceServiceEquityPort", configurationBuilder),
+                    SurveillanceServiceTradeDomain = GetValue("SurveillanceServiceTradeDomain", configurationBuilder),
+                    SurveillanceServiceTradePort = GetValue("SurveillanceServiceTradePort", configurationBuilder),
+                };
 
-            return networkConfiguration;
+                return networkConfiguration;
+            }
         }
 
         public IDataLayerConfiguration BuildDatabaseConfiguration(IConfigurationRoot configurationBuilder)
         {
-            var networkConfiguration = new DataLayerConfiguration
+            lock (_lock)
             {
-                IsEc2Instance = configurationBuilder.GetValue<bool?>("IsEc2Instance") ?? false,
-                ScheduledRuleQueueName = configurationBuilder.GetValue<string>("ScheduledRuleQueueName"),
-                ScheduleRuleDistributedWorkQueueName = configurationBuilder.GetValue<string>("ScheduleRuleDistributedWorkQueueName"),
-                CaseMessageQueueName = configurationBuilder.GetValue<string>("CaseMessageQueueName"),
-                ElasticSearchProtocol = configurationBuilder.GetValue<string>("ElasticSearchProtocol"),
-                ElasticSearchDomain = configurationBuilder.GetValue<string>("ElasticSearchDomain"),
-                ElasticSearchPort = configurationBuilder.GetValue<string>("ElasticSearchPort"),
-                ClientServiceUrl = configurationBuilder.GetValue<string>("ClientServiceUrl"),
-                SurveillanceUserApiAccessToken = configurationBuilder.GetValue<string>("SurveillanceUserApiAccessToken")
-            };
+                Ec2Check(configurationBuilder);
 
-            return networkConfiguration;
+                var networkConfiguration = new DataLayerConfiguration
+                {
+                    IsEc2Instance = Amazon.Util.EC2InstanceMetadata.InstanceId != null,
+                    ScheduledRuleQueueName = GetValue("ScheduledRuleQueueName", configurationBuilder),
+                    ScheduleRuleDistributedWorkQueueName = GetValue("ScheduleRuleDistributedWorkQueueName", configurationBuilder),
+                    CaseMessageQueueName = GetValue("CaseMessageQueueName", configurationBuilder),
+                    ElasticSearchProtocol = GetValue("ElasticSearchProtocol", configurationBuilder),
+                    ElasticSearchDomain = GetValue("ElasticSearchDomain", configurationBuilder),
+                    ElasticSearchPort = GetValue("ElasticSearchPort", configurationBuilder),
+                    ClientServiceUrl = GetValue("ClientServiceUrlAndPort", configurationBuilder),
+                    SurveillanceUserApiAccessToken = GetValue("SurveillanceUserApiAccessToken", configurationBuilder)
+                };
+
+                return networkConfiguration;
+            }
         }
 
         public IRuleConfiguration BuildRuleConfiguration(IConfigurationRoot configurationBuilder)
         {
-            var ruleConfiguration = new RuleConfiguration
+            lock (_lock)
             {
-                CancelledOrderDeduplicationDelaySeconds = configurationBuilder.GetValue<int?>("CancelledOrderDeduplicationDelaySeconds"),
-                AutoScheduleRules = configurationBuilder.GetValue<bool?>("AutoScheduleRules")
-            };
+                Ec2Check(configurationBuilder);
 
-            return ruleConfiguration;
+                var autoScheduleRules = GetValue("AutoScheduleRules", configurationBuilder);
+                bool.TryParse(autoScheduleRules, out var autoScheduleRulesValue);
+
+                var ruleConfiguration = new RuleConfiguration
+                {
+                    AutoScheduleRules = autoScheduleRulesValue
+                };
+
+                return ruleConfiguration;
+            }
         }
 
         public ISystemDataLayerConfig BuildDataLayerConfig(IConfigurationRoot configurationBuilder)
         {
-            var ruleConfiguration = new SystemDataLayerConfig
+            lock (_lock)
             {
-                SurveillanceAuroraConnectionString = configurationBuilder.GetValue<string>("AuroraConnectionString")
+                Ec2Check(configurationBuilder);
+
+                var ruleConfiguration = new SystemDataLayerConfig
+                {
+                    SurveillanceAuroraConnectionString = GetValue("AuroraConnectionString", configurationBuilder)
+                };
+
+                return ruleConfiguration;
+            }
+        }
+
+        private void Ec2Check(IConfigurationRoot configurationBuilder)
+        {
+            if (_hasFetchedEc2Data)
+            {
+                return;
+            }
+
+            var environmentClientId = configurationBuilder.GetValue<string>(DynamoDbKey);
+            _dynamoConfig = FetchEc2Data(environmentClientId);
+        }
+
+        private string GetValue(string field, IConfigurationRoot root)
+        {
+            if (string.IsNullOrWhiteSpace(field))
+            {
+                return string.Empty;
+            }
+
+            field = field.ToLower();
+
+            // ReSharper disable once InvertIf
+            if (_dynamoConfig.ContainsKey(field))
+            {
+                _dynamoConfig.TryGetValue(field, out var value);
+
+                return value ?? string.Empty;
+            }
+
+            return root.GetValue<string>(field);
+        }
+
+        private IDictionary<string, string> FetchEc2Data(string environmentClientId)
+        {
+            var client = new AmazonDynamoDBClient(new AmazonDynamoDBConfig
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.EUWest1,
+                ProxyCredentials = CredentialCache.DefaultCredentials
+            });
+
+            var query = new QueryRequest
+            {
+                TableName = DynamoDbTable,
+                KeyConditionExpression = "#EnvironmentClientDeploymentAttribute = :EnvironmentClientDeploymentValue",
+                ExpressionAttributeNames = new Dictionary<string, string>
+                {
+                    { "#EnvironmentClientDeploymentAttribute", "name" }
+                },
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+                {
+                    {":EnvironmentClientDeploymentValue", new AttributeValue(environmentClientId)},
+                }
             };
 
-            return ruleConfiguration;
+            var attributes = new Dictionary<string, string>();
+            var response = client.QueryAsync(query).Result;
+
+            if (response.Items.Any())
+            {
+                foreach (var item in response.Items.First())
+                {
+                    attributes[item.Key] = item.Value.S;
+                }
+            }
+
+            _hasFetchedEc2Data = true;
+            var casedAttributes = attributes.ToDictionary(i => i.Key?.ToLower(), i => i.Value);
+
+            return casedAttributes;
         }
     }
 }
--- src/Surveillance/Surveillance/Configuration/RuleConfiguration.cs
+++ src/Surveillance/Surveillance/Configuration/RuleConfiguration.cs
@@ -4,10 +4,6 @@ namespace Surveillance.Configuration
 {
     public class RuleConfiguration : IRuleConfiguration
     {
-        /// <summary>
-        /// How long to wait on de duplicating pure subset alerts for cancelled orders
-        /// </summary>
-        public int? CancelledOrderDeduplicationDelaySeconds { get; set; }
         public bool? AutoScheduleRules { get; set; }
     }
 }

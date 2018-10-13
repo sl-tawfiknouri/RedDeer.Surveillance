73e20db455b866a8cc198e1893eefc6ac0fd1cd7
Ryan Trenchard <wintermuteai@outlook.com>
2018-10-13 09:01:04 +0100

    RDPB-2970 small refactor - removed unneeded dependencies from the universe builder

--- src/Surveillance/Surveillance/Universe/UniverseBuilder.cs
+++ src/Surveillance/Surveillance/Universe/UniverseBuilder.cs
@@ -7,9 +7,6 @@ using Domain.Scheduling;
 using Domain.Trades.Orders;
 using Surveillance.DataLayer.Aurora.Market.Interfaces;
 using Surveillance.DataLayer.Aurora.Trade.Interfaces;
-using Surveillance.DataLayer.ElasticSearch.Market.Interfaces;
-using Surveillance.DataLayer.ElasticSearch.Trade.Interfaces;
-using Surveillance.DataLayer.Projectors.Interfaces;
 using Surveillance.Universe.Interfaces;
 using Surveillance.Universe.MarketEvents.Interfaces;
 
@@ -20,28 +17,15 @@ namespace Surveillance.Universe
     /// </summary>
     public class UniverseBuilder : IUniverseBuilder
     {
-        private readonly IRedDeerTradeFormatRepository _tradeRepository;
-        private readonly IReddeerTradeFormatToReddeerTradeFrameProjector _documentProjector;
-
-        private readonly IRedDeerMarketExchangeFormatRepository _equityMarketRepository;
-        private readonly IReddeerMarketExchangeFormatToReddeerExchangeFrameProjector _equityMarketProjector;
         private readonly IReddeerTradeRepository _auroraTradeRepository;
         private readonly IReddeerMarketRepository _auroraMarketRepository;
         private readonly IMarketOpenCloseEventManager _marketManager;
 
         public UniverseBuilder(
-            IRedDeerTradeFormatRepository tradeRepository,
-            IReddeerTradeFormatToReddeerTradeFrameProjector documentProjector,
-            IRedDeerMarketExchangeFormatRepository equityMarketRepository,
-            IReddeerMarketExchangeFormatToReddeerExchangeFrameProjector equityMarketProjector,
             IReddeerTradeRepository auroraTradeRepository,
             IReddeerMarketRepository auroraMarketRepository,
             IMarketOpenCloseEventManager marketManager)
         {
-            _tradeRepository = tradeRepository ?? throw new ArgumentNullException(nameof(tradeRepository));
-            _documentProjector = documentProjector ?? throw new ArgumentNullException(nameof(documentProjector));
-            _equityMarketRepository = equityMarketRepository ?? throw new ArgumentNullException(nameof(equityMarketRepository));
-            _equityMarketProjector = equityMarketProjector ?? throw new ArgumentNullException(nameof(equityMarketProjector));
             _auroraTradeRepository = auroraTradeRepository ?? throw new ArgumentNullException(nameof(auroraTradeRepository));
             _auroraMarketRepository = auroraMarketRepository ?? throw new ArgumentNullException(nameof(auroraMarketRepository));
             _marketManager = marketManager ?? throw new ArgumentNullException(nameof(marketManager));
@@ -60,8 +44,8 @@ namespace Surveillance.Universe
             execution.TimeSeriesInitiation = execution.TimeSeriesInitiation.ToUniversalTime().Date;
             execution.TimeSeriesTermination = execution.TimeSeriesTermination.ToUniversalTime().Date;
 
-            var projectedTrades = await TradeDataFetchAurora(execution); //TradeDataFetch(execution);
-            var exchangeFrames = await MarketEquityDataFetchAurora(execution); // MarketEquityDataFetch(execution);
+            var projectedTrades = await TradeDataFetchAurora(execution);
+            var exchangeFrames = await MarketEquityDataFetchAurora(execution);
             var universe = await UniverseEvents(execution, projectedTrades, exchangeFrames);
 
             return new Universe(projectedTrades, exchangeFrames, universe);
@@ -77,17 +61,6 @@ namespace Surveillance.Universe
             return trades ?? new List<TradeOrderFrame>();
         }
 
-        private async Task<IReadOnlyCollection<TradeOrderFrame>> TradeDataFetch(ScheduledExecution execution)
-        {
-            var trades = await _tradeRepository.Get(
-                execution.TimeSeriesInitiation.DateTime,
-                execution.TimeSeriesTermination.DateTime);
-
-            var projectedTrades = _documentProjector.Project(trades);
-
-            return projectedTrades ?? new List<TradeOrderFrame>();
-        }
-
         private async Task<IReadOnlyCollection<ExchangeFrame>> MarketEquityDataFetchAurora(ScheduledExecution execution)
         {
             var equities =
@@ -96,17 +69,6 @@ namespace Surveillance.Universe
                     execution.TimeSeriesTermination.Date);
 
             return equities ?? new List<ExchangeFrame>();
-        }
-
-        private async Task<IReadOnlyCollection<ExchangeFrame>> MarketEquityDataFetch(ScheduledExecution execution)
-        {
-            var equities = await _equityMarketRepository.Get(
-                execution.TimeSeriesInitiation.DateTime,
-                execution.TimeSeriesTermination.DateTime);
-
-            var projectedEquityData = _equityMarketProjector.Project(equities);
-
-            return projectedEquityData ?? new List<ExchangeFrame>();
         }
 
         private async Task<IReadOnlyCollection<IUniverseEvent>> UniverseEvents(

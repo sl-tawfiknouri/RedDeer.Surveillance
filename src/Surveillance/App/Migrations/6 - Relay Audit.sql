27b4a2988f449de0923ade21c4e8a9d51218cc20
Ryan Trenchard <wintermuteai@outlook.com>
2018-10-27 14:08:29 +0100

    RDPB-3066 initial relay audit script to add upload file tables

---
+++ src/Surveillance/App/Migrations/6 - Relay Audit.sql
@@ -0,0 +1,18 @@
+﻿-- RELAY AUDIT SQL MIGRATION SCRIPT --
+-- AUTHOR : RYAN TRENCHARD
+-- DATE : 27/10/2018
+-- LAST MODIFIED: 27/10/2018 BY RYAN TRENCHARD
+-- JIRA (RDPB-3066)
+-- PURPOSE : Allow for exceptions to be tracked within the relay service (may be called data import in future)
+
+START TRANSACTION;
+
+INSERT INTO Migrations VALUES(6, "Relay Audit.sql", now());
+
+CREATE TABLE SystemProcessOperationUploadFileType(Id int primary key NOT NULL, FileType text);
+INSERT INTO SystemProcessOperationUploadFileType (Id, FileType) VALUES (0, "Market Data File"), (1, "Trade Data File");
+
+CREATE TABLE SystemProcessOperationUploadFile(Id int auto_increment primary key NOT NULL, FileType int, FilePath text, FOREIGN KEY (FileType) REFERENCES SystemProcessOperationUploadFileType(Id));
+
+COMMIT;
+

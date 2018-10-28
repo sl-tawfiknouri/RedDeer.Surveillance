d1182809b07d58d2e731bd544b98d3a9e0eeeefb
Ryan Trenchard <wintermuteai@outlook.com>
2018-10-28 17:00:06 +0000

    RDPB-2989 added a bloomberg ticker migration script to handle larger tickers than I expected

---
+++ src/Surveillance/App/Migrations/7 - Bloomberg Ticker.sql
@@ -0,0 +1,18 @@
+﻿-- BLOOMBERG TICKER FIELD UPDATE SCRIPT --
+-- AUTHOR : RYAN TRENCHARD
+-- DATE : 28/10/2018
+-- LAST MODIFIED: 28/10/2018 BY RYAN TRENCHARD
+-- JIRA (RDPB-3898)
+-- PURPOSE : Update bloomberg ticker field size as its been set too low
+
+START TRANSACTION;
+
+INSERT INTO Migrations VALUES(7, "Bloomberg Ticker.sql", now());
+
+
+ALTER TABLE MarketStockExchangeSecurities MODIFY COLUMN BloombergTicker NVARCHAR(4090);
+ALTER TABLE MarketData MODIFY COLUMN BloombergTicker NVARCHAR(4090);
+ALTER TABLE TradeReddeer MODIFY COLUMN SecurityBloombergTicker NVARCHAR(4090);
+
+COMMIT;
+

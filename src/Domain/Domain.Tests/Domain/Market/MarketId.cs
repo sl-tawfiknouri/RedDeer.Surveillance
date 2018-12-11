e08cf7022342c869fd9896f15e0ce40a22d21d2e
Ryan Trenchard <wintermuteai@outlook.com>
2018-12-11 09:02:40 +0000

    RDPB-3252 removed domain tests

--- src/Domain/Domain.Tests/Domain/Market/MarketId.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Tests.Domain.Market
-{
-    internal class MarketId : global::Domain.Market.Market.MarketId
-    {
-        public MarketId(string id) : base(id)
-        {
-        }
-    }
-}

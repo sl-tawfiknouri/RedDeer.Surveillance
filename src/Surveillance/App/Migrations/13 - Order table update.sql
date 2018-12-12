0a8014387b17f988cf74134d9480de441bf0dbcb
Ryan Trenchard <wintermuteai@outlook.com>
2018-12-12 12:57:19 +0000

    RDPB-3252 table update

---
+++ src/Surveillance/App/Migrations/13 - Order table update.sql
@@ -0,0 +1,12 @@
+﻿-- INITIAL SQL MIGRATION SCRIPT --
+-- AUTHOR : RYAN TRENCHARD
+-- DATE : 12/12/2018
+-- LAST MODIFIED: 12/12/2018 BY RYAN TRENCHARD
+-- PURPOSE : Missing default value for instrument type
+START TRANSACTION;
+
+ INSERT INTO Migrations VALUES(13, "Order table update.sql", now());
+
+ALTER TABLE FinancialInstruments ALTER InstrumentType SET DEFAULT 0;
+
+COMMIT;

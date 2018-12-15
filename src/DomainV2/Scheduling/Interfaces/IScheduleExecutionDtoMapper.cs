66d663439898278ffbd876b78ebe718274ac2ab4
Ryan Trenchard <wintermuteai@outlook.com>
2018-12-15 10:04:32 +0000

    RDPB-3437 updating scheduled execution exposure to serialise/deserialise striaght from to a contract library version

---
+++ src/DomainV2/Scheduling/Interfaces/IScheduleExecutionDtoMapper.cs
@@ -0,0 +1,8 @@
+﻿namespace DomainV2.Scheduling.Interfaces
+{
+    public interface IScheduleExecutionDtoMapper
+    {
+        DomainV2.Scheduling.ScheduledExecution MapToDomain(RedDeer.Contracts.SurveillanceService.Rules.ScheduledExecution dto);
+        RedDeer.Contracts.SurveillanceService.Rules.ScheduledExecution MapToDto(DomainV2.Scheduling.ScheduledExecution dto);
+    }
+}

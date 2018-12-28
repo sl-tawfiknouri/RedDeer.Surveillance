f5c0e51ba5fb4dbb4cc84cb6dbe882f527f05528
Ryan Trenchard <wintermuteai@outlook.com>
2018-12-28 16:34:03 +0000

    RDPB-3479 added test project with tests for all existing logical classes

---
+++ src/ThirdPartySurveillanceDataSynchroniser/ThirdPartySurveillanceDataSynchroniser.Tests/MediatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using ThirdPartySurveillanceDataSynchroniser.Services.Interfaces;
+
+namespace ThirdPartySurveillanceDataSynchroniser.Tests
+{
+    [TestFixture]
+    public class MediatorTests
+    {
+        private ILogger<Mediator> _logger;
+        private IDataRequestsService _dataRequestService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = A.Fake<ILogger<Mediator>>();
+            _dataRequestService = A.Fake<IDataRequestsService>();
+        }
+        
+        [Test]
+        public void Constructor_NullDataRequestsService_IsExceptional()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws<ArgumentNullException>(() => new Mediator(null, _logger));
+        }
+
+        [Test]
+        public void Constructor_NullLogger_IsExceptional()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws<ArgumentNullException>(() => new Mediator(_dataRequestService, null));
+        }
+
+        [Test]
+        public void Initiate_Calls_DataRequestServiceInitiate()
+        {
+            var mediator = new Mediator(_dataRequestService, _logger);
+
+            mediator.Initiate();
+
+            A.CallTo(() => _dataRequestService.Initiate()).MustHaveHappenedOnceExactly();
+        }
+    }
+}
---
+++ src/ThirdPartySurveillanceDataSynchroniser/ThirdPartySurveillanceDataSynchroniser.Tests/Services/DataRequestsServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using ThirdPartySurveillanceDataSynchroniser.Services;
+
+namespace ThirdPartySurveillanceDataSynchroniser.Tests.Services
+{
+    [TestFixture]
+    public class DataRequestsServiceTests
+    {
+        [Test]
+        public void Constructor_NullLogger_IsExceptional()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws<ArgumentNullException>(() => new DataRequestsService(null));
+        }
+    }
+}

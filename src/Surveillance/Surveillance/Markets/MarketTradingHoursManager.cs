1b80039c08a2fb5cbcdf8d7dc5dbfedb8bf6467b
Ryan Trenchard <wintermuteai@outlook.com>
2019-01-01 12:36:35 +0000

    RDPB-3497 market trading hours manager logging added

--- src/Surveillance/Surveillance/Markets/MarketTradingHoursManager.cs
+++ src/Surveillance/Surveillance/Markets/MarketTradingHoursManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 using Surveillance.DataLayer.Api.MarketOpenClose.Interfaces;
 using Surveillance.Markets.Interfaces;
 
@@ -8,29 +9,37 @@ namespace Surveillance.Markets
     public class MarketTradingHoursManager : IMarketTradingHoursManager
     {
         private readonly IMarketOpenCloseApiCachingDecoratorRepository _repository;
+        private readonly ILogger<MarketTradingHoursManager> _logger;
 
-        public MarketTradingHoursManager(IMarketOpenCloseApiCachingDecoratorRepository repository)
+        public MarketTradingHoursManager(
+            IMarketOpenCloseApiCachingDecoratorRepository repository,
+            ILogger<MarketTradingHoursManager> logger)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public ITradingHours Get(string marketIdentifierCode)
         {
             if (string.IsNullOrWhiteSpace(marketIdentifierCode))
             {
+                _logger.LogInformation($"MarketTradingHoursManager received a null or empty MIC {marketIdentifierCode}");
                 return new TradingHours { Mic = marketIdentifierCode, IsValid = false };
             }
 
             var resultTask = _repository.Get();
-            resultTask.Wait();
             var result = resultTask.Result;
 
             var exchange = result.FirstOrDefault(res => string.Equals(res.Code, marketIdentifierCode, StringComparison.InvariantCultureIgnoreCase));
 
             if (exchange == null)
             {
+                _logger.LogError($"MarketTradingHoursManager could not find a match for {marketIdentifierCode}");
+
                 return new TradingHours { Mic = marketIdentifierCode, IsValid = false };
             }
+
+            _logger.LogInformation($"MarketTradingHoursManager found a match for {marketIdentifierCode}");
 
             return new TradingHours
             {

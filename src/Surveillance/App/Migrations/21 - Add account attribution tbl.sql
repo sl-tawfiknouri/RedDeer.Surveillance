cdf70958fa3a6c9e32b2324bd1722ece02c1a002
Ryan Trenchard <wintermuteai@outlook.com>
2019-01-14 09:15:14 +0000

    RDPB-3555 updated account attribution tbl

---
+++ src/Surveillance/App/Migrations/21 - Add account attribution tbl.sql
@@ -0,0 +1,16 @@
+﻿-- SQL MIGRATION SCRIPT --
+-- AUTHOR : RYAN TRENCHARD
+-- DATE : 13/01/2019
+-- LAST MODIFIED: 13/01/2019 BY RYAN TRENCHARD
+-- PURPOSE : Add new fund accounting tables
+
+START TRANSACTION;
+
+    INSERT INTO Migrations VALUES(21, "add account attribution tbl.sql", now());
+	   
+	CREATE TABLE OrdersAttribution(Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, OrderId INT NOT NULL, Fund nvarchar(255), Strategy nvarchar(255), OrderFilledVolume BIGINT);
+
+	CREATE INDEX i_StatusChanged ON Orders (StatusChangedDate);
+    CREATE INDEX i_StatusChanged ON DealerOrders (StatusChangedDate);
+
+COMMIT;

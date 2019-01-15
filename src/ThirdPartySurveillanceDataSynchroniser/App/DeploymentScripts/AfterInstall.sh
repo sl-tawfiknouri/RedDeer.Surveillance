66030d130ce312eebce9ca13c6ccef409ada0b9d
andy smith <andydavidsmith@users.noreply.github.com>
2019-01-15 12:07:26 +0000

    Update AfterInstall.sh

--- src/ThirdPartySurveillanceDataSynchroniser/App/DeploymentScripts/AfterInstall.sh
+++ src/ThirdPartySurveillanceDataSynchroniser/App/DeploymentScripts/AfterInstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Change service folder ownership.
-chown -R reddeer:reddeer /reddeer/thirdpartysurveillancedatasynchroniserservice
-find /reddeer/thirdpartysurveillancedatasynchroniserservice -type d -exec chmod g+s {} \;
+chown -R reddeer:reddeer /reddeer/ ThirdPartySurveillanceDataSynchroniser
+find /reddeer/ ThirdPartySurveillanceDataSynchroniser -type d -exec chmod g+s {} \;
 
 # Create systemd unit file.
-cp /reddeer/thirdpartysurveillancedatasynchroniserservice/thirdpartysurveillancedatasynchroniserservice.service /lib/systemd/system/
+cp /reddeer/ ThirdPartySurveillanceDataSynchroniser/ ThirdPartySurveillanceDataSynchroniser.service /lib/systemd/system/
 
 # Reload daemons and start the new service.
 systemctl daemon-reload
-systemctl enable thirdpartysurveillancedatasynchroniserservice
-systemctl start thirdpartysurveillancedatasynchroniserservice
+systemctl enable  ThirdPartySurveillanceDataSynchroniser
+systemctl start  ThirdPartySurveillanceDataSynchroniser

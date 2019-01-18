56e96ce6a71e255a7e694ca2a58b95df2939a3f7
Ryan Trenchard <wintermuteai@outlook.com>
2019-01-18 17:18:38 +0000

    RDPB-3480 updated validation test runner to check for uploaded order data

--- src/DataImport.Integration.Tests/Validation/ValidationTestRunner.cs
+++ src/DataImport.Integration.Tests/Validation/ValidationTestRunner.cs
@@ -2,19 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using DataImport.Disk_IO.Interfaces;
-using DataImport.Disk_IO.TradeFile;
 using DataImport.Integration.Tests.ObjectGraphs;
-using DataImport.Managers;
-using DataImport.Recorders;
-using DomainV2.Files;
-using DomainV2.Streams;
 using DomainV2.Trading;
 using FakeItEasy;
-using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
-using Surveillance.System.Auditing.Context;
-using Utilities.Disk_IO;
 
 namespace DataImport.Integration.Tests.Validation
 {
@@ -22,8 +13,6 @@ namespace DataImport.Integration.Tests.Validation
     public class ValidationTestRunner
     {
         private readonly IReadOnlyCollection<ValidationFile> _validationFiles;
-        private readonly IUploadTradeFileMonitor _uploadTradeFileMonitor;
-
         public ValidationTestRunner()
         {
             var subPath = Path.Combine("Validation", "Files");
@@ -37,12 +26,102 @@ namespace DataImport.Integration.Tests.Validation
                     6,
                     new List<Func<Order, bool>>
                     {
-                        a => a.OrderId == "943191f1-4144-4b0a-8a48-3c00a2ee20e7",
-                        a => a.OrderId == "9e03994a-c681-4851-b0fa-0da721c0a880",
-                        a => a.OrderId == "e21fd385-c940-46fa-83c8-570b1f0207f3",
-                        a => a.OrderId == "11ced167-82ac-46de-bc78-11dadf689033",
-                        a => a.OrderId == "f1aadd18-d923-4a9f-9d1e-3c5e60dad40a",
+                        a => a.OrderId == "943191f1-4144-4b0a-8a48-3c00a2ee20e7" 
+                             && (int)a.Market.Type == 1
+                            && a.Market.MarketIdentifierCode == "XLON"
+                            && a.Market.Name == "London Stock Exchange"
+                            && a.Instrument.Name == "INTERTEK GROUP PLC  ORD"
+                            && a.Instrument.Cfi == "entspb"
+                            && a.Instrument.IssuerIdentifier == "Intertek Group Plc"
+                            && a.Instrument.Identifiers.Sedol == "3163836"
+                            && a.Instrument.Identifiers.Isin == "GB0031638363"
+                            && a.Instrument.Identifiers.Figi == "BBG000JQZV13"
+                            && a.Instrument.Identifiers.Cusip == "G4911B108"
+                            && a.Instrument.Identifiers.Lei == "8156007259ABDEA3F444"
+                            && a.Instrument.Identifiers.ExchangeSymbol == "ITRK"
+                            && a.Instrument.Identifiers.BloombergTicker == "ITRK LN Equity"
+                            && a.Instrument.Identifiers.ClientIdentifier == "1",
+
+                        a => a.OrderId == "9e03994a-c681-4851-b0fa-0da721c0a880"
+                             && (int)a.Market.Type == 1
+                             && a.Market.MarketIdentifierCode == "XLON"
+                             && a.Market.Name == "London Stock Exchange"
+                             && a.Instrument.Name == "INTERTEK GROUP PLC  ORD"
+                             && a.Instrument.Cfi == "entspb"
+                             && a.Instrument.IssuerIdentifier == "Intertek Group Plc"
+                             && a.Instrument.Identifiers.Sedol == "3163836"
+                             && a.Instrument.Identifiers.Isin == "GB0031638363"
+                             && a.Instrument.Identifiers.Figi == "BBG000JQZV13"
+                             && a.Instrument.Identifiers.Cusip == "G4911B108"
+                             && a.Instrument.Identifiers.Lei == "8156007259ABDEA3F444"
+                             && a.Instrument.Identifiers.ExchangeSymbol == "ITRK"
+                             && a.Instrument.Identifiers.BloombergTicker == "ITRK LN Equity"
+                             && a.Instrument.Identifiers.ClientIdentifier == "2",
+
+                        a => a.OrderId == "e21fd385-c940-46fa-83c8-570b1f0207f3"
+                             && (int)a.Market.Type == 1
+                            && a.Market.MarketIdentifierCode == "XLON"
+                            && a.Market.Name == "London Stock Exchange"
+                            && a.Instrument.Name == "INTERTEK GROUP PLC  ORD"
+                            && a.Instrument.Cfi == "entspb"
+                            && a.Instrument.IssuerIdentifier == "Intertek Group Plc"
+                            && a.Instrument.Identifiers.Sedol == "3163836"
+                            && a.Instrument.Identifiers.Isin == "GB0031638363"
+                            && a.Instrument.Identifiers.Figi == "BBG000JQZV13"
+                            && a.Instrument.Identifiers.Cusip == "G4911B108"
+                            && a.Instrument.Identifiers.Lei == "8156007259ABDEA3F444"
+                            && a.Instrument.Identifiers.ExchangeSymbol == "ITRK"
+                            && a.Instrument.Identifiers.BloombergTicker == "ITRK LN Equity"
+                            && a.Instrument.Identifiers.ClientIdentifier == "3",
+
+
+                        a => a.OrderId == "11ced167-82ac-46de-bc78-11dadf689033"
+                             && (int)a.Market.Type == 1
+                             && a.Market.MarketIdentifierCode == "XLON"
+                             && a.Market.Name == "London Stock Exchange"
+                             && a.Instrument.Name == "INTERTEK GROUP PLC  ORD"
+                             && a.Instrument.Cfi == "entspb"
+                             && a.Instrument.IssuerIdentifier == "Intertek Group Plc"
+                             && a.Instrument.Identifiers.Sedol == "3163836"
+                             && a.Instrument.Identifiers.Isin == "GB0031638363"
+                             && a.Instrument.Identifiers.Figi == "BBG000JQZV13"
+                             && a.Instrument.Identifiers.Cusip == "G4911B108"
+                             && a.Instrument.Identifiers.Lei == "8156007259ABDEA3F444"
+                             && a.Instrument.Identifiers.ExchangeSymbol == "ITRK"
+                             && a.Instrument.Identifiers.BloombergTicker == "ITRK LN Equity"
+                             && a.Instrument.Identifiers.ClientIdentifier == "4",
+
+                        a => a.OrderId == "f1aadd18-d923-4a9f-9d1e-3c5e60dad40a"
+                             && (int)a.Market.Type == 1
+                             && a.Market.MarketIdentifierCode == "XLON"
+                             && a.Market.Name == "London Stock Exchange"
+                             && a.Instrument.Name == "INTERTEK GROUP PLC  ORD"
+                             && a.Instrument.Cfi == "entspb"
+                             && a.Instrument.IssuerIdentifier == "Intertek Group Plc"
+                             && a.Instrument.Identifiers.Sedol == "3163836"
+                             && a.Instrument.Identifiers.Isin == "GB0031638363"
+                             && a.Instrument.Identifiers.Figi == "BBG000JQZV13"
+                             && a.Instrument.Identifiers.Cusip == "G4911B108"
+                             && a.Instrument.Identifiers.Lei == "8156007259ABDEA3F444"
+                             && a.Instrument.Identifiers.ExchangeSymbol == "ITRK"
+                             && a.Instrument.Identifiers.BloombergTicker == "ITRK LN Equity"
+                             && a.Instrument.Identifiers.ClientIdentifier == "5",
+
                         a => a.OrderId == "6afdc02d-c1fd-4782-ab8a-9fd97ac4b698"
+                             && (int)a.Market.Type == 1
+                             && a.Market.MarketIdentifierCode == "XLON"
+                             && a.Market.Name == "London Stock Exchange"
+                             && a.Instrument.Name == "INTERTEK GROUP PLC  ORD"
+                             && a.Instrument.Cfi == "entspb"
+                             && a.Instrument.IssuerIdentifier == "Intertek Group Plc"
+                             && a.Instrument.Identifiers.Sedol == "3163836"
+                             && a.Instrument.Identifiers.Isin == "GB0031638363"
+                             && a.Instrument.Identifiers.Figi == "BBG000JQZV13"
+                             && a.Instrument.Identifiers.Cusip == "G4911B108"
+                             && a.Instrument.Identifiers.Lei == "8156007259ABDEA3F444"
+                             && a.Instrument.Identifiers.ExchangeSymbol == "ITRK"
+                             && a.Instrument.Identifiers.BloombergTicker == "ITRK LN Equity"
+                             && a.Instrument.Identifiers.ClientIdentifier == "6"
                     })
             };
         }

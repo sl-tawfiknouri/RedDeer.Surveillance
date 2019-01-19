5a57695cda267ae489b00cfb6578754dfe2e8682
Ryan Trenchard <wintermuteai@outlook.com>
2019-01-19 10:19:57 +0000

    RDPB-3610 drop the old trade reddeer table and added an enum table to support QA analysis

--- src/Surveillance/App/Migrations/22 - Add two fields to orders tbl.sql
+++ src/Surveillance/App/Migrations/22 - Add two fields to orders tbl.sql
@@ -14,4 +14,15 @@ START TRANSACTION;
 	ALTER TABLE DealerOrders ADD CreatedDate DateTime NOT NULL DEFAULT now();
 	ALTER TABLE DealerOrders ADD LifeCycleStatus INT NOT NULL DEFAULT 0;
 
+	DROP TABLE TradeReddeer;
+
+	CREATE TABLE OrderLifeCycleStatus (Id INT NOT NULL PRIMARY KEY, Description nvarchar(255));
+	INSERT INTO OrderLifeCycleStatus(Id, Description) VALUES (0, "Unknown");
+	INSERT INTO OrderLifeCycleStatus(Id, Description) VALUES (1, "Placed");
+	INSERT INTO OrderLifeCycleStatus(Id, Description) VALUES (2, "Booked");
+	INSERT INTO OrderLifeCycleStatus(Id, Description) VALUES (3, "Amended");
+	INSERT INTO OrderLifeCycleStatus(Id, Description) VALUES (4, "Rejected");
+	INSERT INTO OrderLifeCycleStatus(Id, Description) VALUES (5, "Cancelled");
+	INSERT INTO OrderLifeCycleStatus(Id, Description) VALUES (6, "Filled");
+
 COMMIT;

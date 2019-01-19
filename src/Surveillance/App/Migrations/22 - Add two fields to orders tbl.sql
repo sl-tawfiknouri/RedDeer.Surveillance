f95307056e8996ab07da3d446dd21c5ce2b760d0
Ryan Trenchard <wintermuteai@outlook.com>
2019-01-19 08:57:40 +0000

    RDPB-3610 added created on date and LifeCycleStatus fields

---
+++ src/Surveillance/App/Migrations/22 - Add two fields to orders tbl.sql
@@ -0,0 +1,14 @@
+﻿-- SQL MIGRATION SCRIPT --
+-- AUTHOR : RYAN TRENCHARD
+-- DATE : 19/01/2019
+-- LAST MODIFIED: 19/01/2019 BY RYAN TRENCHARD
+-- PURPOSE : Extend orders with two new fields to support the Client Service
+
+START TRANSACTION;
+
+    INSERT INTO Migrations VALUES(22, "Add two fields to orders tbl.sql", now());
+	   
+	ALTER TABLE Orders ADD CreatedDate DateTime NOT NULL DEFAULT now();
+	ALTER TABLE ORDERS ADD LifeCycleStatus INT NOT NULL DEFAULT 0;
+
+COMMIT;

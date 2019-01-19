b8620aefaefde6bde9bdd18e8222393900dd0bc2
Ryan Trenchard <wintermuteai@outlook.com>
2019-01-19 15:53:34 +0000

    RDPB-3611 add a script for trader names

---
+++ src/Surveillance/App/Migrations/24 - Add trader name to order dealer order.sql
@@ -0,0 +1,14 @@
+﻿-- SQL MIGRATION SCRIPT --
+-- AUTHOR : RYAN TRENCHARD
+-- DATE : 19/01/2019
+-- LAST MODIFIED: 19/01/2019 BY RYAN TRENCHARD
+-- PURPOSE : Add trader name to order / dealer order as per E.W. request.
+
+START TRANSACTION;
+
+    INSERT INTO Migrations VALUES(24, "Add trader name to order dealer order.sql", now());
+
+	ALTER TABLE Orders ADD TraderName nvarchar(255);
+	ALTER TABLE DealerOrders ADD TraderName nvarchar(255);
+
+COMMIT;

4bfb01b8e666e0282e2628cbef705106a67842a2
Ryan Trenchard <wintermuteai@outlook.com>
2019-01-27 11:07:35 +0000

    RDPB-3648 found combo of project versions that actually works for specflow

---
+++ src/Surveillance.Specflow.Tests/TestSteps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Surveillance.Specflow.Tests
+{
+    [Binding]
+    public class TestSteps
+    {
+       
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+
+        }
+
+    }
+}

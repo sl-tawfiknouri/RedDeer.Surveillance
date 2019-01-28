3e2d8d6709e96e7a4001a7e141b5a3c5c3af7bd2
Ryan Trenchard <wintermuteai@outlook.com>
2019-01-28 17:01:47 +0000

    RDPB-3468 merge changes

--- src/Surveillance/Surveillance/Rules/MarkingTheClose/MarkingTheCloseBreach.cs
+++ src/Surveillance/Surveillance/Rules/MarkingTheClose/MarkingTheCloseBreach.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using DomainV2.Financial;
 using DomainV2.Trading;
 using Surveillance.Rules.MarkingTheClose.Interfaces;
-using Surveillance.System.Auditing.Context.Interfaces;
+using Surveillance.Systems.Auditing.Context.Interfaces;
 using Surveillance.Trades;
 using Surveillance.Trades.Interfaces;
 using Surveillance.Universe.MarketEvents;

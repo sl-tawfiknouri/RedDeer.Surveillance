e6b38fc5659249c786e9302724311f874b7698be
Ryan Trenchard <wintermuteai@outlook.com>
2019-02-05 16:27:38 +0000

    RDPB-3695 updated intraday cache with method for processing date ranges

--- src/Surveillance/Surveillance/Markets/Interfaces/IUniverseEquityIntradayCache.cs
+++ src/Surveillance/Surveillance/Markets/Interfaces/IUniverseEquityIntradayCache.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using DomainV2.Equity.TimeBars;
+using DomainV2.Financial;
 using DomainV2.Markets;
+using Surveillance.Rules;
 
 namespace Surveillance.Markets.Interfaces
 {
@@ -10,5 +12,9 @@ namespace Surveillance.Markets.Interfaces
         void Add(EquityIntraDayTimeBarCollection value);
         MarketDataResponse<EquityInstrumentIntraDayTimeBar> GetForLatestDayOnly(MarketDataRequest request);
         MarketDataResponse<List<EquityInstrumentIntraDayTimeBar>> GetMarkets(MarketDataRequest request);
+        MarketDataResponse<List<EquityInstrumentIntraDayTimeBar>> GetMarketsForRange(
+            MarketDataRequest request,
+            IReadOnlyCollection<DateRange> dates,
+            RuleRunMode runMode);
     }
 }

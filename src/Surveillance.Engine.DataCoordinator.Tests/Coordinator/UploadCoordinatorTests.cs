1b24e26b2bab9a9d5a0be1b03d003f5985ac222b
Ryan Trenchard <wintermuteai@outlook.com>
2019-02-14 12:56:51 +0000

    RDPB-3675 added some guard clauses for analyse file id

--- src/Surveillance.Engine.DataCoordinator.Tests/Coordinator/UploadCoordinatorTests.cs
+++ src/Surveillance.Engine.DataCoordinator.Tests/Coordinator/UploadCoordinatorTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using DomainV2.Contracts;
 using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
 using Surveillance.Engine.DataCoordinator.Configuration.Interfaces;
 using Surveillance.Engine.DataCoordinator.Coordinator;
@@ -10,19 +13,48 @@ namespace Surveillance.Engine.DataCoordinator.Tests.Coordinator
     public class UploadCoordinatorTests
     {
         private IRuleConfiguration _ruleConfiguration;
+        private ILogger<UploadCoordinator> _logger;
 
         [SetUp]
         public void Setup()
         {
             _ruleConfiguration = A.Fake<IRuleConfiguration>();
+            A.CallTo(() => _ruleConfiguration.AutoScheduleRules).Returns(true);
+
+            _logger = new NullLogger<UploadCoordinator>();
         }
 
         [Test]
         public void Constructor_RuleConfiguration_Null_Is_Exceptional()
         {
             // ReSharper disable once ObjectCreationAsStatement
-            Assert.Throws<ArgumentNullException>(() => new UploadCoordinator(null));
+            Assert.Throws<ArgumentNullException>(() => new UploadCoordinator(null, _logger));
         }
 
+        [Test]
+        public void Constructor_Logger_Null_Is_Exceptional()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws<ArgumentNullException>(() => new UploadCoordinator(_ruleConfiguration, null));
+        }
+
+        [Test]
+        public void AnalyseFileId_Null_Message_Returns()
+        {
+            var coordinator = new UploadCoordinator(_ruleConfiguration, _logger);
+
+            Assert.DoesNotThrow(() => coordinator.AnalyseFileId(null));
+        }
+
+        [Test]
+        public void AnalyseFileId_OkMessage_No_AutoScheduling_Returns()
+        {
+            A.CallTo(() => _ruleConfiguration.AutoScheduleRules).Returns(false);
+            var coordinator = new UploadCoordinator(_ruleConfiguration, _logger);
+
+            var message = new UploadCoordinatorMessage {FileId = Guid.NewGuid().ToString()};
+
+            Assert.DoesNotThrow(() => coordinator.AnalyseFileId(message));
+        }
     }
 }
--- src/Surveillance.Engine.DataCoordinator/Coordinator/UploadCoordinator.cs
+++ src/Surveillance.Engine.DataCoordinator/Coordinator/UploadCoordinator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DomainV2.Contracts;
+using Microsoft.Extensions.Logging;
 using Surveillance.Engine.DataCoordinator.Configuration.Interfaces;
 using Surveillance.Engine.DataCoordinator.Coordinator.Interfaces;
 
@@ -8,15 +9,33 @@ namespace Surveillance.Engine.DataCoordinator.Coordinator
     public class UploadCoordinator : IUploadCoordinator
     {
         private readonly IRuleConfiguration _ruleConfiguration;
+        private readonly ILogger<UploadCoordinator> _logger;
 
-        public UploadCoordinator(IRuleConfiguration ruleConfiguration)
+        public UploadCoordinator(
+            IRuleConfiguration ruleConfiguration,
+            ILogger<UploadCoordinator> logger)
         {
             _ruleConfiguration = ruleConfiguration ?? throw new ArgumentNullException(nameof(ruleConfiguration));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public void AnalyseFileId(UploadCoordinatorMessage message)
         {
+            if (message == null
+                || string.IsNullOrWhiteSpace(message.FileId))
+            {
+                _logger.LogInformation($"UploadCoordinator AnalyseFileId received a null message. Exiting.");
+                return;
+            }
 
+            if (_ruleConfiguration.AutoScheduleRules == null
+                || !_ruleConfiguration.AutoScheduleRules.GetValueOrDefault(false))
+            {
+                _logger.LogInformation($"UploadCoordinator AnalyseFileId received a message with a file id of {message.FileId} but auto scheduling was turned off. Exiting.");
+                return;
+            }
+
+            
         }
     }
 }

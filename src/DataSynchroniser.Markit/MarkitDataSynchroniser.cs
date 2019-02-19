9d1ab6b05daadd980546268aa1b8b75545d35ef2
Ryan Trenchard <wintermuteai@outlook.com>
2019-02-19 08:25:20 +0000

    RDPB-3816 initial open/close logging for the markit handler

--- src/DataSynchroniser.Markit/MarkitDataSynchroniser.cs
+++ src/DataSynchroniser.Markit/MarkitDataSynchroniser.cs
@@ -1,19 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using DataSynchroniser.Api.Markit.Interfaces;
 using Domain.Markets;
+using Microsoft.Extensions.Logging;
 using Surveillance.Auditing.Context.Interfaces;
 
 namespace DataSynchroniser.Api.Markit
 {
     public class MarkitDataSynchroniser : IMarkitDataSynchroniser
     {
+        private readonly ILogger<MarkitDataSynchroniser> _logger;
+
+        public MarkitDataSynchroniser(ILogger<MarkitDataSynchroniser> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public async Task Handle(
             string systemProcessOperationId,
             ISystemProcessOperationThirdPartyDataRequestContext dataRequestContext,
             IReadOnlyCollection<MarketDataRequest> marketDataRequests)
         {
-            
+            _logger.LogInformation($"MarkitDataSynchroniser Handle processing request");
+
+
+
+            _logger.LogInformation($"MarkitDataSynchroniser Handle completed processing request");
         }
     }
 }

18ebe34b4363abb47b5bde5f3564b03c5869536d
Ryan Trenchard <wintermuteai@outlook.com>
2019-02-21 12:48:33 +0000

    RDPB-3828 added a blank wash trade rule for fixed income

---
+++ src/Surveillance.Engine/Rules/FixedIncome/WashTrade/FixedIncomeWashTradeRule.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Surveillance.Auditing.Context.Interfaces;
+using Surveillance.Engine.Rules.Factories.Interfaces;
+using Surveillance.Engine.Rules.Rules.FixedIncome.WashTrade.Interfaces;
+using Surveillance.Engine.Rules.Trades;
+using Surveillance.Engine.Rules.Trades.Interfaces;
+using Surveillance.Engine.Rules.Universe.Interfaces;
+using Surveillance.Engine.Rules.Universe.MarketEvents;
+
+namespace Surveillance.Engine.Rules.Rules.FixedIncome.WashTrade
+{
+    public class FixedIncomeWashTradeRule : BaseUniverseRule, IFixedIncomeWashTradeRule
+    {
+        private readonly ILogger<FixedIncomeWashTradeRule> _logger;
+
+        public FixedIncomeWashTradeRule(
+            TimeSpan windowSize,
+            ISystemProcessOperationRunRuleContext ruleCtx,
+            IUniverseMarketCacheFactory marketCacheFactory,
+            RuleRunMode runMode,
+            ILogger<FixedIncomeWashTradeRule> logger,
+            ILogger<TradingHistoryStack> tradingStackLogger)
+            : base(
+                windowSize,
+                Domain.Scheduling.Rules.FixedIncomeWashTrades,
+                Versioner.Version(1, 0),
+                $"{nameof(FixedIncomeWashTradeRule)}",
+                ruleCtx,
+                marketCacheFactory,
+                runMode,
+                logger,
+                tradingStackLogger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        protected override IUniverseEvent Filter(IUniverseEvent value)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RunRule(ITradingHistoryStack history)
+        {
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} RunRule called at {UniverseDateTime}");
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} RunRule completed for {UniverseDateTime}");
+        }
+
+        protected override void RunInitialSubmissionRule(ITradingHistoryStack history)
+        {
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} RunInitialSubmissionRule called at {UniverseDateTime}");
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} RunInitialSubmissionRule completed for {UniverseDateTime}");
+        }
+
+        protected override void Genesis()
+        {
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} Genesis called at {UniverseDateTime}");
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} Genesis completed for {UniverseDateTime}");
+        }
+
+        protected override void MarketOpen(MarketOpenClose exchange)
+        {
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} MarketOpen called at {UniverseDateTime}");
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} MarketOpen completed for {UniverseDateTime}");
+        }
+
+        protected override void MarketClose(MarketOpenClose exchange)
+        {
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} MarketClose called at {UniverseDateTime}");
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} MarketClose completed for {UniverseDateTime}");
+        }
+
+        protected override void EndOfUniverse()
+        {
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} EndOfUniverse called at {UniverseDateTime}");
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} EndOfUniverse completed for {UniverseDateTime}");
+        }
+
+        public object Clone()
+        {
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} Clone called at {UniverseDateTime}");
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeWashTradeRule)} Clone completed for {UniverseDateTime}");
+
+            throw new NotImplementedException();
+        }
+    }
+}

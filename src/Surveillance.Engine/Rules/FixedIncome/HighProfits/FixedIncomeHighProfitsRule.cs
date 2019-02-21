25eb0cc4f61e4ddc953a4c161967da6bbee1ae44
Ryan Trenchard <wintermuteai@outlook.com>
2019-02-21 11:58:36 +0000

    RDPB-3828 added empty high profits fixed income rule

--- src/Surveillance.Engine/Rules/FixedIncome/HighProfits/FixedIncomeHighProfitsRule.cs
+++ src/Surveillance.Engine/Rules/FixedIncome/HighProfits/FixedIncomeHighProfitsRule.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Surveillance.Auditing.Context.Interfaces;
 using Surveillance.Engine.Rules.Factories.Interfaces;
+using Surveillance.Engine.Rules.Rules.FixedIncome.HighProfits.Interfaces;
 using Surveillance.Engine.Rules.Trades;
 using Surveillance.Engine.Rules.Trades.Interfaces;
 using Surveillance.Engine.Rules.Universe.Interfaces;
@@ -9,7 +10,7 @@ using Surveillance.Engine.Rules.Universe.MarketEvents;
 
 namespace Surveillance.Engine.Rules.Rules.FixedIncome.HighProfits
 {
-    public class FixedIncomeHighProfitsRule : BaseUniverseRule
+    public class FixedIncomeHighProfitsRule : BaseUniverseRule, IFixedIncomeHighProfitsRule
     {
         private readonly ILogger<FixedIncomeHighProfitsRule> _logger;
 
@@ -41,31 +42,66 @@ namespace Surveillance.Engine.Rules.Rules.FixedIncome.HighProfits
 
         protected override void RunRule(ITradingHistoryStack history)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} RunRule called at {UniverseDateTime}");
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} RunRule completed for {UniverseDateTime}");
         }
 
         protected override void RunInitialSubmissionRule(ITradingHistoryStack history)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} RunInitialSubmissionRule called at {UniverseDateTime}");
+
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} RunInitialSubmissionRule completed for {UniverseDateTime}");
         }
 
         protected override void Genesis()
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Universe Genesis called at {UniverseDateTime}");
+
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Universe Genesis completed for {UniverseDateTime}");
         }
 
         protected override void MarketOpen(MarketOpenClose exchange)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Market Open called at {UniverseDateTime} for {exchange?.MarketId}");
+
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Market Open completed at {UniverseDateTime} for {exchange?.MarketId}");
         }
 
         protected override void MarketClose(MarketOpenClose exchange)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Market Close called at {UniverseDateTime} for {exchange?.MarketId}");
+
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Market Close completed at {UniverseDateTime} for {exchange?.MarketId}");
         }
 
         protected override void EndOfUniverse()
         {
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Eschaton called at {UniverseDateTime}");
+
+
+
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Eschaton completed for {UniverseDateTime}");
+
+        }
+
+        public object Clone()
+        {
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Clone called at {UniverseDateTime}");
+
+           
+
+            _logger.LogInformation($"{nameof(FixedIncomeHighProfitsRule)} Clone completed for {UniverseDateTime}");
+
             throw new NotImplementedException();
         }
     }

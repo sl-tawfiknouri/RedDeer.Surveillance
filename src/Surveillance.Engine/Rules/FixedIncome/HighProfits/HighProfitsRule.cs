1b86f1df5722d58249b4dc27b2b69998054e4076
Ryan Trenchard <wintermuteai@outlook.com>
2019-02-21 09:26:54 +0000

    RDPB-3825 empty fixed income high profit rule added

---
+++ src/Surveillance.Engine/Rules/FixedIncome/HighProfits/HighProfitsRule.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Surveillance.Auditing.Context.Interfaces;
+using Surveillance.Engine.Rules.Factories.Interfaces;
+using Surveillance.Engine.Rules.Trades;
+using Surveillance.Engine.Rules.Trades.Interfaces;
+using Surveillance.Engine.Rules.Universe.Interfaces;
+using Surveillance.Engine.Rules.Universe.MarketEvents;
+
+namespace Surveillance.Engine.Rules.Rules.FixedIncome.HighProfits
+{
+    public class HighProfitsRule : BaseUniverseRule
+    {
+        public HighProfitsRule(
+            TimeSpan windowSize,
+            Domain.Scheduling.Rules rules,
+            ISystemProcessOperationRunRuleContext ruleCtx,
+            IUniverseMarketCacheFactory marketCacheFactory,
+            RuleRunMode runMode,
+            ILogger<HighProfitsRule> logger,
+            ILogger<TradingHistoryStack> tradingStackLogger)
+            : base(
+                windowSize,
+                rules,
+                Versioner.Version(1, 0),
+                "Fixed Income High Profits Rule",
+                ruleCtx,
+                marketCacheFactory,
+                runMode,
+                logger,
+                tradingStackLogger)
+        {
+        }
+
+        protected override IUniverseEvent Filter(IUniverseEvent value)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RunRule(ITradingHistoryStack history)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RunInitialSubmissionRule(ITradingHistoryStack history)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Genesis()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void MarketOpen(MarketOpenClose exchange)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void MarketClose(MarketOpenClose exchange)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void EndOfUniverse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

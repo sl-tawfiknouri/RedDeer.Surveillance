5d4b677bf314fb8344c2102544fc35a27131963a
Ryan Trenchard <wintermuteai@outlook.com>
2019-02-22 14:46:44 +0000

    RDPB-3828 implemented the fixed income high profit factory

--- src/Surveillance.Engine/Factories/FixedIncome/FixedIncomeHighProfitFactory.cs
+++ src/Surveillance.Engine/Factories/FixedIncome/FixedIncomeHighProfitFactory.cs
@@ -1,14 +1,49 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
+using Surveillance.Auditing.Context.Interfaces;
 using Surveillance.Engine.Rules.Factories.FixedIncome.Interfaces;
+using Surveillance.Engine.Rules.Factories.Interfaces;
+using Surveillance.Engine.Rules.RuleParameters.FixedIncome.Interfaces;
+using Surveillance.Engine.Rules.Rules;
+using Surveillance.Engine.Rules.Rules.FixedIncome.HighProfits;
 using Surveillance.Engine.Rules.Rules.FixedIncome.HighProfits.Interfaces;
+using Surveillance.Engine.Rules.Trades;
+using Surveillance.Engine.Rules.Universe.Filter.Interfaces;
 
 namespace Surveillance.Engine.Rules.Factories.FixedIncome
 {
     public class FixedIncomeHighProfitFactory : IFixedIncomeHighProfitFactory
     {
-        public IFixedIncomeHighProfitsRule BuildRule()
+        private readonly IUniverseFixedIncomeOrderFilter _fixedIncomeOrderFilter;
+        private readonly IUniverseMarketCacheFactory _marketCacheFactory;
+        private readonly ILogger<FixedIncomeHighProfitsRule> _logger;
+        private readonly ILogger<TradingHistoryStack> _stackLogger;
+
+        public FixedIncomeHighProfitFactory(
+            IUniverseFixedIncomeOrderFilter fixedIncomeOrderFilter,
+            IUniverseMarketCacheFactory marketCacheFactory,
+            ILogger<FixedIncomeHighProfitsRule> logger, 
+            ILogger<TradingHistoryStack> stackLogger)
         {
-            throw new ArgumentNullException();
+            _fixedIncomeOrderFilter = fixedIncomeOrderFilter ?? throw new ArgumentNullException(nameof(fixedIncomeOrderFilter));
+            _marketCacheFactory = marketCacheFactory ?? throw new ArgumentNullException(nameof(marketCacheFactory));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _stackLogger = stackLogger ?? throw new ArgumentNullException(nameof(stackLogger));
+        }
+
+        public IFixedIncomeHighProfitsRule BuildRule(
+            IHighProfitsRuleFixedIncomeParameters parameters,
+            ISystemProcessOperationRunRuleContext ruleCtx,
+            RuleRunMode runMode)
+        {
+            return new FixedIncomeHighProfitsRule(
+                parameters,
+                _fixedIncomeOrderFilter,
+                ruleCtx,
+                _marketCacheFactory,
+                runMode,
+                _logger,
+                _stackLogger);
         }
     }
 }
--- src/Surveillance.Engine/Factories/FixedIncome/Interfaces/IFixedIncomeHighProfitFactory.cs
+++ src/Surveillance.Engine/Factories/FixedIncome/Interfaces/IFixedIncomeHighProfitFactory.cs
@@ -1,9 +1,15 @@
-﻿using Surveillance.Engine.Rules.Rules.FixedIncome.HighProfits.Interfaces;
+﻿using Surveillance.Auditing.Context.Interfaces;
+using Surveillance.Engine.Rules.RuleParameters.FixedIncome.Interfaces;
+using Surveillance.Engine.Rules.Rules;
+using Surveillance.Engine.Rules.Rules.FixedIncome.HighProfits.Interfaces;
 
 namespace Surveillance.Engine.Rules.Factories.FixedIncome.Interfaces
 {
     public interface IFixedIncomeHighProfitFactory
     {
-        IFixedIncomeHighProfitsRule BuildRule();
+        IFixedIncomeHighProfitsRule BuildRule(
+            IHighProfitsRuleFixedIncomeParameters parameters,
+            ISystemProcessOperationRunRuleContext ruleCtx,
+            RuleRunMode runMode);
     }
 }

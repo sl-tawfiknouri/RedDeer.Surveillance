41d894029dbcc5469a64d9c80fa7450d8de64f37
Ryan Trenchard <wintermuteai@outlook.com>
2019-03-04 15:37:09 +0000

    RDPB-3876 currencyvalue to money in high volume rule breach

--- src/Surveillance.Engine/Rules/Equity/HighVolume/HighVolumeRule.cs
+++ src/Surveillance.Engine/Rules/Equity/HighVolume/HighVolumeRule.cs
@@ -332,10 +332,10 @@ namespace Surveillance.Engine.Rules.Rules.Equity.HighVolume
 
             if (tradedValue >= thresholdValue)
             {
-                var thresholdCurrencyValue = new Money((decimal)thresholdValue, mostRecentTrade.OrderCurrency);
-                var tradedCurrencyValue = new Money((decimal)tradedValue, mostRecentTrade.OrderCurrency);
+                var thresholdMoney = new Money((decimal)thresholdValue, mostRecentTrade.OrderCurrency);
+                var tradedMoney = new Money((decimal)tradedValue, mostRecentTrade.OrderCurrency);
 
-                return new HighVolumeRuleBreach.BreachDetails(true, breachPercentage, thresholdCurrencyValue, tradedCurrencyValue);
+                return new HighVolumeRuleBreach.BreachDetails(true, breachPercentage, thresholdMoney, tradedMoney);
             }
 
             return HighVolumeRuleBreach.BreachDetails.None();

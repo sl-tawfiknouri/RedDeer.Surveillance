0c15a48baa1243e5835a23643c91d96287f9a343
Ryan Trenchard <wintermuteai@outlook.com>
2019-03-05 14:15:03 +0000

    RDPB-3886 updated holding to have total price paid

--- src/Domain.Core/Trading/Holding.cs
+++ src/Domain.Core/Trading/Holding.cs
@@ -4,15 +4,17 @@ namespace Domain.Core.Trading
 {
     public class Holding
     {
-        public Holding(FinancialInstrument instrument, Money averagePricePaid, long size)
+        public Holding(FinancialInstrument instrument, Money averagePricePaid, Money totalPricePaid, long size)
         {
             Instrument = instrument;
             AveragePricePaid = averagePricePaid;
+            TotalPricePaid = totalPricePaid;
             Size = size;
         }
 
         public FinancialInstrument Instrument { get; }
         public Money AveragePricePaid { get; }
+        public Money TotalPricePaid { get; }
         public long Size { get; }
     }
 }

163dde3d180c90f350142672e1f41d1987a28e01
Ryan Trenchard <wintermuteai@outlook.com>
2019-03-05 15:34:04 +0000

    RDPB-3886 holding sold price added

--- src/Domain.Core/Trading/Holding.cs
+++ src/Domain.Core/Trading/Holding.cs
@@ -4,17 +4,27 @@ namespace Domain.Core.Trading
 {
     public class Holding
     {
-        public Holding(FinancialInstrument instrument, Money averagePricePaid, Money totalPricePaid, long size)
+        public Holding(
+            FinancialInstrument instrument,
+            Money averagePricePaid,
+            Money totalPricePaid,
+            Money averagePriceSold,
+            Money totalPriceSold,
+            long size)
         {
             Instrument = instrument;
             AveragePricePaid = averagePricePaid;
             TotalPricePaid = totalPricePaid;
+            AveragePriceSold = averagePriceSold;
+            TotalPriceSold = totalPricePaid;
             Size = size;
         }
 
         public FinancialInstrument Instrument { get; }
         public Money AveragePricePaid { get; }
         public Money TotalPricePaid { get; }
+        public Money AveragePriceSold { get; }
+        public Money TotalPriceSold { get; }
         public long Size { get; }
     }
 }

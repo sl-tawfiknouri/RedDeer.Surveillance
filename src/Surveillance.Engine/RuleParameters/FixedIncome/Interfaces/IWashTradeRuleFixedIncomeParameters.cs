f65ebfd80ee9a425a93046a0f3f83d1a606893a8
Ryan Trenchard <wintermuteai@outlook.com>
2019-03-05 12:59:36 +0000

    RDPB-3886 updated wash trade dto references and populated additional parameters on wash trade parameter

--- src/Surveillance.Engine/RuleParameters/FixedIncome/Interfaces/IWashTradeRuleFixedIncomeParameters.cs
+++ src/Surveillance.Engine/RuleParameters/FixedIncome/Interfaces/IWashTradeRuleFixedIncomeParameters.cs
@@ -6,5 +6,18 @@ namespace Surveillance.Engine.Rules.RuleParameters.FixedIncome.Interfaces
     public interface IWashTradeRuleFixedIncomeParameters : IFilterableRule, IRuleParameter, IOrganisationalFactorable
     {
         TimeSpan WindowSize { get; }
+
+        bool PerformAveragePositionAnalysis { get; }
+        bool PerformClusteringPositionAnalysis { get; }
+
+        // Parameter set one
+        int? AveragePositionMinimumNumberOfTrades { get; }
+        decimal? AveragePositionMaximumPositionValueChange { get; }
+        decimal? AveragePositionMaximumAbsoluteValueChangeAmount { get; }
+        string AveragePositionMaximumAbsoluteValueChangeCurrency { get; }
+
+        // Parameter set two
+        int? ClusteringPositionMinimumNumberOfTrades { get; }
+        decimal? ClusteringPercentageValueDifferenceThreshold { get; }
     }
 }
--- src/Surveillance.Specflow.Tests/StepDefinitions/Fi.WashTrade/FixedIncomeWashTradeApiParameters.cs
+++ src/Surveillance.Specflow.Tests/StepDefinitions/Fi.WashTrade/FixedIncomeWashTradeApiParameters.cs
@@ -3,5 +3,16 @@
     public class FixedIncomeWashTradeApiParameters
     {
         public int WindowHours { get; set; }
+
+        public bool PerformAveragePositionAnalysis { get; set; }
+        public bool PerformClusteringPositionAnalysis { get; set; }
+
+        public int? AveragePositionMinimumNumberOfTrades { get; set; }
+        public decimal? AveragePositionMaximumPositionValueChange { get; set; }
+        public decimal? AveragePositionMaximumAbsoluteValueChangeAmount { get; set; }
+        public string AveragePositionMaximumAbsoluteValueChangeCurrency { get; set; }
+
+        public int? ClusteringPositionMinimumNumberOfTrades { get; set; }
+        public decimal? ClusteringPercentageValueDifferenceThreshold { get; set; }
     }
 }

ed14501ed9019246ec8ebf98607ab3a007bace7b
Ryan Trenchard <wintermuteai@outlook.com>
2019-03-07 15:59:36 +0000

    RDPB-3876 created a live rules service

---
+++ src/Domain.Surveillance/Rules/Interfaces/ILiveRulesService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Domain.Surveillance.Scheduling;
+
+namespace Domain.Surveillance.Rules
+{
+    public interface ILiveRulesService
+    {
+        IReadOnlyCollection<Scheduling.Rules> LiveRules();
+        bool RuleIsLive(Scheduling.Rules rule);
+        IReadOnlyCollection<Scheduling.Rules> UnliveRules();
+    }
+}
---
+++ src/Domain.Surveillance/Rules/LiveRulesService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain.Surveillance.Rules
+{
+    public class LiveRulesService : ILiveRulesService
+    {
+        private readonly IReadOnlyCollection<Scheduling.Rules> _liveRules =
+            new List<Scheduling.Rules>
+            {
+                Scheduling.Rules.HighVolume,
+                Scheduling.Rules.CancelledOrders,
+                Scheduling.Rules.HighProfits,
+                Scheduling.Rules.MarkingTheClose,
+                Scheduling.Rules.WashTrade,
+                Scheduling.Rules.FixedIncomeWashTrades,
+                Scheduling.Rules.FixedIncomeHighProfits,
+                Scheduling.Rules.FixedIncomeHighVolumeIssuance
+            };
+
+        public bool RuleIsLive(Scheduling.Rules rule)
+        {
+            return _liveRules.Contains(rule);
+        }
+
+        public IReadOnlyCollection<Scheduling.Rules> LiveRules()
+        {
+            return _liveRules;
+        }
+
+        public IReadOnlyCollection<Scheduling.Rules> UnliveRules()
+        {
+            var rules = Enum.GetValues(typeof(Scheduling.Rules));
+            var castRules = rules.Cast<Scheduling.Rules>().ToList();
+            var ruleList = new List<Scheduling.Rules>(castRules);
+
+            return ruleList.Where(i => !_liveRules.Contains(i)).ToList();
+        }
+    }
+}

4bb19478f3931020b65c893255c22a2bd3fb47de
Ryan Trenchard <wintermuteai@outlook.com>
2019-04-10 15:47:04 +0100

    RDPB-3941 sql script added

---
+++ src/Surveillance/App/Migrations/29 - Add etl file type to enum tbl.sql
@@ -0,0 +1,12 @@
+﻿-- SQL MIGRATION SCRIPT --
+-- AUTHOR : RYAN TRENCHARD
+-- DATE : 10/04/2019
+-- LAST MODIFIED: 10/04/2019 BY RYAN TRENCHARD
+-- PURPOSE : Extend file type enum
+
+START TRANSACTION;
+
+    INSERT INTO Migrations VALUES(29, "Add file type enum value.sql", now());
+	INSERT INTO SystemProcessOperationUploadFileType VALUES(3, "Etl Data File");
+
+COMMIT;

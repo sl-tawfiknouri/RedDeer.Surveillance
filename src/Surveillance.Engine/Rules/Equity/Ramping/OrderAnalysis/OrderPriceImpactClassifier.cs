cdf1ddadc4956e801b9a9120a01eecec5c09e4af
Ryan Trenchard <wintermuteai@outlook.com>
2019-05-03 14:33:34 +0100

    RDPB-4034 implementing order price impact classifier

--- src/Surveillance.Engine/Rules/Equity/Ramping/OrderAnalysis/OrderPriceImpactClassifier.cs
+++ src/Surveillance.Engine/Rules/Equity/Ramping/OrderAnalysis/OrderPriceImpactClassifier.cs
@@ -15,7 +15,29 @@ namespace Surveillance.Engine.Rules.Rules.Equity.Ramping.OrderAnalysis
                 return new PriceImpactSummary(PriceImpactClassification.Unknown, segment);
             }
 
-            return new PriceImpactSummary(PriceImpactClassification.Positive, segment);
+            var buySide = orders.Count(_ => 
+                _.OrderDirection == OrderDirections.BUY
+                || _.OrderDirection == OrderDirections.COVER);
+
+            if (buySide == 0)
+            {
+                return new PriceImpactSummary(PriceImpactClassification.Negative, segment);
+            }
+
+            var proportionBuySide = (decimal)buySide / (decimal)orders.Count;
+
+            if (proportionBuySide >= 0.66m)
+            {
+                return new PriceImpactSummary(PriceImpactClassification.Positive, segment);
+            }
+            else if (proportionBuySide <= 0.33m)
+            {
+                return new PriceImpactSummary(PriceImpactClassification.Negative, segment);
+            }
+            else
+            {
+                return new PriceImpactSummary(PriceImpactClassification.Unknown, segment);
+            }
         }
 
         public IPriceImpactSummary ClassifyByWeightedVolume(IReadOnlyCollection<Order> orders, TimeSegment segment)
@@ -25,7 +47,33 @@ namespace Surveillance.Engine.Rules.Rules.Equity.Ramping.OrderAnalysis
                 return new PriceImpactSummary(PriceImpactClassification.Unknown, segment);
             }
 
-            return new PriceImpactSummary(PriceImpactClassification.Positive, segment);
+            var buySide =
+                    orders
+                        .Where(_ =>
+                            _.OrderDirection == OrderDirections.BUY
+                            || _.OrderDirection == OrderDirections.COVER)
+                        .Sum(_ => _.OrderFilledVolume ?? 0);
+
+            if (buySide == 0)
+            {
+                return new PriceImpactSummary(PriceImpactClassification.Negative, segment);
+            }
+
+            var totalVolume = orders.Sum(_ => _.OrderFilledVolume ?? 0);
+            var proportionBuySide = (decimal)buySide / (decimal)totalVolume;
+
+            if (proportionBuySide >= 0.66m)
+            {
+                return new PriceImpactSummary(PriceImpactClassification.Positive, segment);
+            }
+            else if (proportionBuySide <= 0.33m)
+            {
+                return new PriceImpactSummary(PriceImpactClassification.Negative, segment);
+            }
+            else
+            {
+                return new PriceImpactSummary(PriceImpactClassification.Unknown, segment);
+            }
         }
     }
 }

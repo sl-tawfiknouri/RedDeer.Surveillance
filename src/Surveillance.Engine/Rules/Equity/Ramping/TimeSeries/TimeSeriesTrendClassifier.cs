eaf1f6f014152ea92ee3bf1c5f846a85511ca28c
Ryan Trenchard <wintermuteai@outlook.com>
2019-05-07 10:02:00 +0100

    RDPB-4034 time series trend classifier very (very) basic implementation

--- src/Surveillance.Engine/Rules/Equity/Ramping/TimeSeries/TimeSeriesTrendClassifier.cs
+++ src/Surveillance.Engine/Rules/Equity/Ramping/TimeSeries/TimeSeriesTrendClassifier.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Domain.Core.Financial.Assets.Interfaces;
 using Domain.Core.Markets.Timebars;
+using Microsoft.Extensions.Logging;
 using Surveillance.Engine.Rules.Rules.Equity.Ramping.TimeSeries.Interfaces;
 
 namespace Surveillance.Engine.Rules.Rules.Equity.Ramping.TimeSeries
 {
     public class TimeSeriesTrendClassifier : ITimeSeriesTrendClassifier
     {
+        private readonly ILogger<TimeSeriesTrendClassifier> _logger;
+
+        public TimeSeriesTrendClassifier(
+            ILogger<TimeSeriesTrendClassifier> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public ITimeSeriesTrendClassification Classify(
             List<EquityInstrumentIntraDayTimeBar> timeBars,
             IFinancialInstrument financialInstrument, 
@@ -15,15 +25,58 @@ namespace Surveillance.Engine.Rules.Rules.Equity.Ramping.TimeSeries
             DateTime termination,
             TimeSegment timeSegment)
         {
-            // ok so we have t1 -> t2
-            // how do we classify the time series?
-            // simple p@t1 >= p@t2
+            if (timeBars == null
+                || !timeBars.Any())
+            {
+                _logger?.LogWarning($"Received an empty or null time bars collection");
 
-            // first things first lets get the raw data in here..
+                return new TimeSeriesTrendClassification(
+                    financialInstrument,
+                    TimeSeriesTrend.Unclassified,
+                    commencement,
+                    termination,
+                    timeSegment);
+            }
+
+            // basic implementation for initial work - we will make this more sophisticated in response to tests
+            var initial = timeBars.FirstOrDefault();
+            var final = timeBars.LastOrDefault();
+
+            if (initial == null
+                || final == null)
+            {
+                return new TimeSeriesTrendClassification(
+                    financialInstrument,
+                    TimeSeriesTrend.Unclassified,
+                    commencement,
+                    termination,
+                    timeSegment);
+            }
+
+            // TODO take a volatiltiy measure and return chaotic if volatiltiy is high
+            if (final.SpreadTimeBar.Price.Value > initial.SpreadTimeBar.Price.Value)
+            {
+                return new TimeSeriesTrendClassification(
+                    financialInstrument,
+                    TimeSeriesTrend.Increasing,
+                    commencement,
+                    termination,
+                    timeSegment);
+            }
+
+            if (final.SpreadTimeBar.Price.Value < initial.SpreadTimeBar.Price.Value)
+            {
+                return new TimeSeriesTrendClassification(
+                    financialInstrument,
+                    TimeSeriesTrend.Decreasing,
+                    commencement,
+                    termination,
+                    timeSegment);
+            }
 
             return new TimeSeriesTrendClassification(
                 financialInstrument,
-                TimeSeriesTrend.Increasing,
+                TimeSeriesTrend.Unclassified,
                 commencement,
                 termination,
                 timeSegment);

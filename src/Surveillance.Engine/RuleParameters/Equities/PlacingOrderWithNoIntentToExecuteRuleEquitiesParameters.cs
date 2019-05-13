7b4e3f811d57aea2eb606bec6b840176226d63e3
Ryan Trenchard <wintermuteai@outlook.com>
2019-05-13 17:24:39 +0100

    RDPB-4066 rule parameters updating contract to incorporate new dto from reddeer sln

--- src/Surveillance.Engine/RuleParameters/Equities/PlacingOrderWithNoIntentToExecuteRuleEquitiesParameters.cs
+++ src/Surveillance.Engine/RuleParameters/Equities/PlacingOrderWithNoIntentToExecuteRuleEquitiesParameters.cs
@@ -6,77 +6,68 @@ using Surveillance.Engine.Rules.RuleParameters.OrganisationalFactors;
 
 namespace Surveillance.Engine.Rules.RuleParameters.Equities
 {
-    public class LayeringRuleEquitiesParameters : ILayeringRuleEquitiesParameters
+    public class PlacingOrderWithNoIntentToExecuteRuleEquitiesParameters 
+        : IPlacingOrderWithNoIntentToExecuteRuleEquitiesParameters
     {
-        public LayeringRuleEquitiesParameters(
-            string id,
+        public PlacingOrderWithNoIntentToExecuteRuleEquitiesParameters(
+            string id, 
+            decimal sigma,
             TimeSpan windowSize,
-            decimal? percentageOfMarketDailyVolume,
-            decimal? percentOfMarketWindowVolume,
-            bool? checkForCorrespondingPriceMovement,
             IReadOnlyCollection<ClientOrganisationalFactors> factors,
             bool aggregateNonFactorableIntoOwnCategory)
         {
             Id = id ?? string.Empty;
 
+            Sigma = sigma;
             WindowSize = windowSize;
-            PercentageOfMarketDailyVolume = percentageOfMarketDailyVolume;
-            PercentageOfMarketWindowVolume = percentOfMarketWindowVolume;
-            CheckForCorrespondingPriceMovement = checkForCorrespondingPriceMovement;
+            Factors = factors ?? new ClientOrganisationalFactors[0];
+            AggregateNonFactorableIntoOwnCategory = aggregateNonFactorableIntoOwnCategory;
 
             Accounts = RuleFilter.None();
             Traders = RuleFilter.None();
             Markets = RuleFilter.None();
             Funds = RuleFilter.None();
             Strategies = RuleFilter.None();
-
-            Factors = factors ?? new ClientOrganisationalFactors[0];
-            AggregateNonFactorableIntoOwnCategory = aggregateNonFactorableIntoOwnCategory;
         }
 
-        public LayeringRuleEquitiesParameters(
+        public PlacingOrderWithNoIntentToExecuteRuleEquitiesParameters(
             string id,
+            decimal sigma,
             TimeSpan windowSize,
-            decimal? percentageOfMarketDailyVolume,
-            decimal? percentOfMarketWindowVolume,
-            bool? checkForCorrespondingPriceMovement,
+            IReadOnlyCollection<ClientOrganisationalFactors> factors,
+            bool aggregateNonFactorableIntoOwnCategory,
             RuleFilter accounts,
             RuleFilter traders,
             RuleFilter markets,
             RuleFilter funds,
-            RuleFilter strategies,
-            IReadOnlyCollection<ClientOrganisationalFactors> factors,
-            bool aggregateNonFactorableIntoOwnCategory)
+            RuleFilter strategies)
         {
             Id = id ?? string.Empty;
 
+            Sigma = sigma;
             WindowSize = windowSize;
-            PercentageOfMarketDailyVolume = percentageOfMarketDailyVolume;
-            PercentageOfMarketWindowVolume = percentOfMarketWindowVolume;
-            CheckForCorrespondingPriceMovement = checkForCorrespondingPriceMovement;
+            Factors = factors ?? new ClientOrganisationalFactors[0];
+            AggregateNonFactorableIntoOwnCategory = aggregateNonFactorableIntoOwnCategory;
 
             Accounts = accounts ?? RuleFilter.None();
             Traders = traders ?? RuleFilter.None();
             Markets = markets ?? RuleFilter.None();
             Funds = funds ?? RuleFilter.None();
             Strategies = strategies ?? RuleFilter.None();
-
-            Factors = factors ?? new ClientOrganisationalFactors[0];
-            AggregateNonFactorableIntoOwnCategory = aggregateNonFactorableIntoOwnCategory;
         }
 
         public string Id { get; }
+        public decimal Sigma { get; }
         public TimeSpan WindowSize { get; }
-        public decimal? PercentageOfMarketDailyVolume { get; }
-        public decimal? PercentageOfMarketWindowVolume { get; }
-        public bool? CheckForCorrespondingPriceMovement { get; }
+
+        public IReadOnlyCollection<ClientOrganisationalFactors> Factors { get; set; }
+        public bool AggregateNonFactorableIntoOwnCategory { get; set; }
+
         public RuleFilter Accounts { get; set; }
         public RuleFilter Traders { get; set; }
         public RuleFilter Markets { get; set; }
         public RuleFilter Funds { get; set; }
         public RuleFilter Strategies { get; set; }
-        public IReadOnlyCollection<ClientOrganisationalFactors> Factors { get; set; }
-        public bool AggregateNonFactorableIntoOwnCategory { get; set; }
 
         public bool HasFilters()
         {

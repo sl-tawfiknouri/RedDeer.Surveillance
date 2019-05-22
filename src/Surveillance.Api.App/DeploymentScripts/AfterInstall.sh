9e7133841f6228d778309f91058d0d159ed8be6b
Dan Butler <dan.butler@reddeer.com>
2019-05-22 09:27:36 +0100

    Missed name change in afterinstall script

--- src/Surveillance.Api.App/DeploymentScripts/AfterInstall.sh
+++ src/Surveillance.Api.App/DeploymentScripts/AfterInstall.sh
@@ -5,7 +5,7 @@ chown -R reddeer:reddeer /reddeer/surveillanceapiservice
 find /reddeer/surveillanceapiservice -type d -exec chmod g+s {} \;
 
 # Create systemd unit file.
-cp /reddeer/surveillanceservice/surveillanceapiservice.service /lib/systemd/system/
+cp /reddeer/surveillanceapiservice/surveillanceapiservice.service /lib/systemd/system/
 
 # Reload daemons and start the new service.
 systemctl daemon-reload

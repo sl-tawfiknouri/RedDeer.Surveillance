baa3ef9cfd729caad79a95f48a8099deb5e13108
Ryan Trenchard <wintermuteai@outlook.com>
2019-06-11 16:14:02 +0100

    RDPB-4115 added sqs enum and a dto for schedule request ad hoc

---
+++ src/Domain.Surveillance/Scheduling/AdHocScheduleRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Domain.Surveillance.Aws;
+
+namespace Domain.Surveillance.Scheduling
+{
+    /// <summary>
+    /// Should be JSON serialisable
+    /// </summary>
+    [Serializable]
+    public class AdHocScheduleRequest
+    {
+        public DateTime ScheduleFor { get; set; }
+        public SurveillanceSqsQueue Queue { get; set; }
+        public string JsonSqsMessage { get; set; }
+        public string OriginatingService { get; set; }
+    }
+}

3cbe87eb0f33972e5705fa6826c7fd18baf3000b
mantas-masidlauskas <mantas.masidlauskas@gmail.com>
2019-06-12 12:43:50 +0100

    Order contract update. Was causing Cast issue.

--- src/Surveillance.Api.Client/Dtos/OrderDto.cs
+++ src/Surveillance.Api.Client/Dtos/OrderDto.cs
@@ -1,8 +1,6 @@
 ﻿using RedDeer.Surveillance.Api.Client.Enums;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 
 namespace RedDeer.Surveillance.Api.Client.Dtos
 {
@@ -22,7 +20,7 @@ namespace RedDeer.Surveillance.Api.Client.Dtos
         public decimal? LimitPrice { get; set; }
         public decimal? AverageFillPrice { get; set; }
         public long? OrderedVolume { get; set; }
-        public long FilledVolume { get; set; }
+        public long? FilledVolume { get; set; }
         public string ClearingAgent { get; set; }
         public string DealingInstructions { get; set; }
         public decimal? OptionStrikePrice { get; set; }

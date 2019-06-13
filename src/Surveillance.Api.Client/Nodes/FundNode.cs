5a67fe316a87710039c3ebe72bd7ff0d6b0acb13
mantas-masidlauskas <mantas.masidlauskas@gmail.com>
2019-06-13 18:06:05 +0100

    Exposing ClientAccount RuleFilter clasificator values

---
+++ src/Surveillance.Api.Client/Nodes/FundNode.cs
@@ -0,0 +1,11 @@
+﻿using RedDeer.Surveillance.Api.Client.Infrastructure;
+
+namespace RedDeer.Surveillance.Api.Client.Nodes
+{
+    public class FundNode : Node<FundNode>
+    {
+        public FundNode(Parent parent) : base(parent) { }
+
+        public FundNode FieldName() => AddField("name");
+    }
+}

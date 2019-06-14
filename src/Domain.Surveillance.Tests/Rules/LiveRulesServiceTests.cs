fd71016aff7a21ac59d73c1f5afdef0350cb84c1
Ryan Trenchard <wintermuteai@outlook.com>
2019-06-14 15:25:03 +0100

    RDPB-4066 live rules service test updated

--- src/Domain.Surveillance.Tests/Rules/LiveRulesServiceTests.cs
+++ src/Domain.Surveillance.Tests/Rules/LiveRulesServiceTests.cs
@@ -10,6 +10,7 @@ namespace Domain.Surveillance.Tests.Rules
         [TestCase(Scheduling.Rules.HighVolume, true)]
         [TestCase(Scheduling.Rules.CancelledOrders, true)]
         [TestCase(Scheduling.Rules.WashTrade, true)]
+        [TestCase(Scheduling.Rules.PlacingOrderWithNoIntentToExecute, true)]
         [TestCase(Scheduling.Rules.FixedIncomeHighProfits, true)]
         [TestCase(Scheduling.Rules.FixedIncomeHighVolumeIssuance, true)]
         [TestCase(Scheduling.Rules.UniverseFilter, false)]
@@ -36,12 +37,14 @@ namespace Domain.Surveillance.Tests.Rules
             Assert.IsTrue(result.Contains(Scheduling.Rules.HighProfits));
             Assert.IsTrue(result.Contains(Scheduling.Rules.WashTrade));
             Assert.IsTrue(result.Contains(Scheduling.Rules.CancelledOrders));
+            Assert.IsTrue(result.Contains(Scheduling.Rules.PaintingTheTape));
+            Assert.IsTrue(result.Contains(Scheduling.Rules.PlacingOrderWithNoIntentToExecute));
+
 
             Assert.IsFalse(result.Contains(Scheduling.Rules.UniverseFilter));
             Assert.IsFalse(result.Contains(Scheduling.Rules.PumpAndDump));
             Assert.IsFalse(result.Contains(Scheduling.Rules.TrashAndCash));
             Assert.IsFalse(result.Contains(Scheduling.Rules.CrossAssetManipulation));
-            Assert.IsFalse(result.Contains(Scheduling.Rules.PaintingTheTape));
         }
 
         [Test]
@@ -55,13 +58,15 @@ namespace Domain.Surveillance.Tests.Rules
             Assert.IsTrue(result.Contains(Scheduling.Rules.PumpAndDump));
             Assert.IsTrue(result.Contains(Scheduling.Rules.TrashAndCash));
             Assert.IsTrue(result.Contains(Scheduling.Rules.CrossAssetManipulation));
-            Assert.IsTrue(result.Contains(Scheduling.Rules.PaintingTheTape));
 
+            Assert.IsFalse(result.Contains(Scheduling.Rules.PlacingOrderWithNoIntentToExecute));
             Assert.IsFalse(result.Contains(Scheduling.Rules.HighVolume));
             Assert.IsFalse(result.Contains(Scheduling.Rules.FixedIncomeWashTrades));
             Assert.IsFalse(result.Contains(Scheduling.Rules.HighProfits));
             Assert.IsFalse(result.Contains(Scheduling.Rules.WashTrade));
             Assert.IsFalse(result.Contains(Scheduling.Rules.CancelledOrders));
+            Assert.IsFalse(result.Contains(Scheduling.Rules.PaintingTheTape));
+
         }
     }
 }

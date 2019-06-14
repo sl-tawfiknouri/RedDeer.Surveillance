10ed670ce09564e57f37e5de322cb7f49de7a074
mantas-masidlauskas <mantas.masidlauskas@gmail.com>
2019-06-14 13:03:20 +0100

    Exposing Traders RuleFilter clasificator values

---
+++ src/Surveillance.Api.Client/Queries/TraderQuery.cs
@@ -0,0 +1,25 @@
+﻿using RedDeer.Surveillance.Api.Client.Dtos;
+using RedDeer.Surveillance.Api.Client.Filters;
+using RedDeer.Surveillance.Api.Client.Infrastructure;
+using RedDeer.Surveillance.Api.Client.Nodes;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedDeer.Surveillance.Api.Client.Queries
+{
+    public class TraderQuery : Query<List<TraderDto>>
+    {
+        public TraderFilter<TraderNode> Filter { get; }
+
+        public TraderQuery()
+        {
+            Filter = new TraderFilter<TraderNode>(new TraderNode(this));
+        }
+
+        internal override async Task<List<TraderDto>> HandleAsync(IRequest request, CancellationToken ctx)
+        {
+            return await BuildAndPost<List<TraderDto>>("traders", Filter, request, ctx);
+        }
+    }
+}

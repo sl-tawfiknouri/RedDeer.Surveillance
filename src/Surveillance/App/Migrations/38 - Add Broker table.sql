751dfcc3817798c52f4b044508e5e81943eefdd3
mantas-masidlauskas <mantas.masidlauskas@gmail.com>
2019-06-22 10:01:44 +0100

    Add Broker Migration script
    (cherry picked from commit f4d1d5fdb9577dcd3a033ba8d8c277859d77b283)

---
+++ src/Surveillance/App/Migrations/38 - Add Broker table.sql
@@ -0,0 +1,24 @@
+﻿-- SQL MIGRATION SCRIPT --
+-- PURPOSE : Add Broker table
+
+START TRANSACTION;
+
+    INSERT INTO Migrations 
+		VALUES(38, "Add Broker table", now());
+	
+	CREATE TABLE Brokers
+	( 
+		Id INT NOT NULL AUTO_INCREMENT,
+		ExternalId NVARCHAR(64) NULL,
+		Name NVARCHAR(1024) NULL,
+		PRIMARY KEY (Id)
+	);
+		
+	ALTER TABLE Orders 
+		ADD BrokerId INT NULL;
+
+	ALTER TABLE Orders 
+		ADD CONSTRAINT fk_Orders_Brokers_BrokerId 
+		FOREIGN KEY (BrokerId) REFERENCES Brokers(Id);
+
+COMMIT;

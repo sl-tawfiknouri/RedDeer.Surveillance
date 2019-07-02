3334934d82b5e5043b2c19e5ed70bd1c57c62b60
Ryan Trenchard <wintermuteai@outlook.com>
2019-07-02 14:36:14 +0100

    RDPB-4237 added a broker enrichment flag to sql

---
+++ src/Surveillance/App/Migrations/43 - Add broker enrichment flag.sql
@@ -0,0 +1,11 @@
+﻿-- CLEAR ENRICHMENT STATUS OF ALL FINANCIALINSTRUMENTS --
+-- AUTHOR : Ryan Trenchard
+-- DATE : 02/07/2019
+-- PURPOSE : Add live flag to broker field to show data that has or hasn't been enriched
+START TRANSACTION;
+
+INSERT INTO Migrations VALUES(43, "Add broker enrichment flag.sql", now());
+
+	ALTER TABLE Brokers ADD Live bit;
+
+COMMIT;

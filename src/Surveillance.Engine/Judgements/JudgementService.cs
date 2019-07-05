edc0a6039b17b5d2f475ccc7fabdf0984c578aaa
Ryan Trenchard <wintermuteai@outlook.com>
2019-07-05 15:41:58 +0100

    RDPB-4170 judgement service updated to save values

--- src/Surveillance.Engine/Judgements/JudgementService.cs
+++ src/Surveillance.Engine/Judgements/JudgementService.cs
@@ -20,13 +20,72 @@ namespace Surveillance.Engine.Rules.Judgements
                 _logger?.LogError($"High Profit Judgement was null");
                 return;
             }
-
-            // save high profit judgement
-            // send high profit judgements onward to database
-            // something to retrieve judgements -sort them into db
-
-
-
         }
+
+        public void Judgement(CancelledOrderJudgement cancelledOrder)
+        {
+            if (cancelledOrder == null)
+            {
+                _logger?.LogError($"Cancelled Order Judgement was null");
+                return;
+            }
+        }
+
+        public void Judgement(HighVolumeJudgement highVolume)
+        {
+            if (highVolume == null)
+            {
+                _logger?.LogError($"High Volume Judgement was null");
+                return;
+            }
+        }
+
+        public void Judgement(LayeringJudgement layering)
+        {
+            if (layering == null)
+            {
+                _logger?.LogError($"Layering Judgement was null");
+                return;
+            }
+        }
+
+        public void Judgement(MarkingTheCloseJudgement markingTheClose)
+        {
+            if (markingTheClose == null)
+            {
+                _logger?.LogError($"Marking The Close Judgement was null");
+                return;
+            }
+        }
+
+        public void Judgement(PlacingOrdersWithNoIntentToExecuteJudgement placingOrders)
+        {
+            if (placingOrders == null)
+            {
+                _logger?.LogError($"Placing Orders Judgement was null");
+                return;
+            }
+        }
+
+        public void Judgement(RampingJudgement ramping)
+        {
+            if (ramping == null)
+            {
+                _logger?.LogError($"Ramping Judgement was null");
+                return;
+            }
+        }
+
+        public void Judgement(SpoofingJudgement spoofing)
+        {
+            if (spoofing == null)
+            {
+                _logger?.LogError($"Spoofing Judgement was null");
+                return;
+            }
+        }
+
+
+
     }
 }

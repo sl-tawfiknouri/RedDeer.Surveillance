e0770d83746427f8d759f52ab4ac84d40822ce44
Ryan Trenchard <wintermuteai@outlook.com>
2019-07-15 11:56:28 +0100

    RDPB-4256 added high volume filter interface

---
+++ src/Surveillance.Engine/Universe/Filter/Interfaces/IHighVolumeFilter.cs
@@ -0,0 +1,12 @@
+﻿using Surveillance.Engine.Rules.Universe.Interfaces;
+
+namespace Surveillance.Engine.Rules.Universe.Filter.Interfaces
+{
+    /// <summary>
+    /// High volume as a (%) of venue trading
+    /// </summary>
+    public interface IHighVolumeFilter
+    {
+        bool Filter(IUniverseEvent universeEvent);
+    }
+}

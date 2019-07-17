4eda9db224ab45dcaec7d8382d71963ae6e9305c
Ryan Trenchard <wintermuteai@outlook.com>
2019-07-17 15:58:18 +0100

    RDPB-4256 saving some test progress, going to switch to using specflow for this

--- src/Surveillance.Engine.Rules.Tests/Universe/Filter/HighVolumeVenueFilterTests.cs
+++ src/Surveillance.Engine.Rules.Tests/Universe/Filter/HighVolumeVenueFilterTests.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Linq;
 using FakeItEasy;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
+using RedDeer.Contracts.SurveillanceService.Api.Markets;
 using Surveillance.Auditing.Context.Interfaces;
+using Surveillance.DataLayer.Aurora.BMLL;
+using Surveillance.DataLayer.Aurora.Interfaces;
+using Surveillance.Engine.Rules.Factories;
 using Surveillance.Engine.Rules.Factories.Interfaces;
+using Surveillance.Engine.Rules.Markets;
 using Surveillance.Engine.Rules.Markets.Interfaces;
 using Surveillance.Engine.Rules.RuleParameters;
 using Surveillance.Engine.Rules.RuleParameters.Filter;
 using Surveillance.Engine.Rules.Rules;
 using Surveillance.Engine.Rules.Trades;
+using Surveillance.Engine.Rules.Universe;
 using Surveillance.Engine.Rules.Universe.Filter;
 using Surveillance.Engine.Rules.Universe.Filter.Interfaces;
+using Surveillance.Engine.Rules.Universe.Interfaces;
+using Surveillance.Reddeer.ApiClient.MarketOpenClose.Interfaces;
 
 namespace Surveillance.Engine.Rules.Tests.Universe.Filter
 {
@@ -23,7 +33,10 @@ namespace Surveillance.Engine.Rules.Tests.Universe.Filter
         private IUniverseOrderFilter _universeOrderFilter;
         private TimeWindows _timeWindows;
         private DecimalRangeRuleFilter _decimalRangeRuleFilter;
+        // plain fake
         private IMarketTradingHoursService _marketTradingHoursService;
+        // populated with mocked ops
+        private IMarketTradingHoursService _tradingHoursService;
         private ILogger _baseLogger;
         private ILogger<TradingHistoryStack> _tradingLogger;
         private ILogger<HighVolumeVenueFilter> _logger;
@@ -41,6 +54,47 @@ namespace Surveillance.Engine.Rules.Tests.Universe.Filter
             _baseLogger = A.Fake<ILogger>();
             _tradingLogger = A.Fake<ILogger<TradingHistoryStack>>();
             _logger = A.Fake<ILogger<HighVolumeVenueFilter>>();
+
+            A
+                .CallTo(() => _universeOrderFilter.Filter(A<IUniverseEvent>.Ignored))
+                .ReturnsLazily(_ => _.Arguments.First() as IUniverseEvent);
+
+            var repository = A.Fake<IMarketOpenCloseApiCachingDecorator>();
+
+            A
+                .CallTo(() => repository.Get()).
+                Returns(new ExchangeDto[]
+                {
+                    new ExchangeDto
+                    {
+                        Code = "XLON",
+                        MarketOpenTime = TimeSpan.FromHours(8),
+                        MarketCloseTime = TimeSpan.FromHours(16),
+                        IsOpenOnMonday = true,
+                        IsOpenOnTuesday = true,
+                        IsOpenOnWednesday = true,
+                        IsOpenOnThursday = true,
+                        IsOpenOnFriday = true,
+                        IsOpenOnSaturday = true,
+                        IsOpenOnSunday = true,
+                    },
+
+                    new ExchangeDto
+                    {
+                        Code = "NASDAQ",
+                        MarketOpenTime = TimeSpan.FromHours(15),
+                        MarketCloseTime = TimeSpan.FromHours(23),
+                        IsOpenOnMonday = true,
+                        IsOpenOnTuesday = true,
+                        IsOpenOnWednesday = true,
+                        IsOpenOnThursday = true,
+                        IsOpenOnFriday = true,
+                        IsOpenOnSaturday = true,
+                        IsOpenOnSunday = true,
+                    }
+                });
+
+            _tradingHoursService = new MarketTradingHoursService(repository, new NullLogger<MarketTradingHoursService>());
         }
 
         [Test]

09ea938769afe46d3a31a3a7c57dadc3f2da721f
Ryan Trenchard <wintermuteai@outlook.com>
2019-08-17 13:11:44 +0100

    RDPB-0000 culture info constant test added

---
+++ src/Domain.Core.Tests/Extensions/CultureInfoConstantsTests.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Core.Extensions;
+using NUnit.Framework;
+
+namespace Domain.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class CultureInfoConstantsTests
+    {
+        [Test]
+        public void DefaultCultureInfo_Is_EnGb()
+        {
+            var defaultCulture = CultureInfoConstants.DefaultCultureInfo;
+
+            Assert.AreEqual(defaultCulture, "en-GB");
+        }
+    }
+}

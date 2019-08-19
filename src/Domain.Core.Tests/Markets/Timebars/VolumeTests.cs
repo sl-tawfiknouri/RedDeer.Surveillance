91f275eca9298234e0e52b5a7413d283d6878f05
Ryan Trenchard <wintermuteai@outlook.com>
2019-08-19 08:25:07 +0100

    RDPB-0000 volume test added (mostly fluff for test coverage %)

---
+++ src/Domain.Core.Tests/Markets/Timebars/VolumeTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Core.Tests.Markets.Timebars
+{
+    using Domain.Core.Markets.Timebars;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class VolumeTests
+    {
+        [Test]
+        public void Ctor_AssignsVariable_Correctly()
+        {
+            var vol = new Volume(100);
+
+            Assert.AreEqual(100, vol.Traded);
+        }
+    }
+}

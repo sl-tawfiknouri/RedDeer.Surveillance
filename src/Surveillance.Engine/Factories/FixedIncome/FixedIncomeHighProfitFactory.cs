9f54ca31fc876681800acbf79a8215336fab7f58
Ryan Trenchard <wintermuteai@outlook.com>
2019-09-15 12:00:45 +0100

    RDPB-4568 style changes

--- src/Surveillance.Engine/Factories/FixedIncome/FixedIncomeHighProfitFactory.cs
+++ src/Surveillance.Engine/Factories/FixedIncome/FixedIncomeHighProfitFactory.cs
@@ -21,24 +21,78 @@
     using Surveillance.Engine.Rules.Trades;
     using Surveillance.Engine.Rules.Universe.Filter.Interfaces;
 
+    /// <summary>
+    /// The fixed income high profit factory.
+    /// </summary>
     public class FixedIncomeHighProfitFactory : IFixedIncomeHighProfitFactory
     {
+        /// <summary>
+        /// The fixed income order filter service.
+        /// </summary>
         private readonly IUniverseFixedIncomeOrderFilterService fixedIncomeOrderFilterService;
 
+        /// <summary>
+        /// The logger.
+        /// </summary>
         private readonly ILogger<FixedIncomeHighProfitsRule> logger;
 
+        /// <summary>
+        /// The market cache factory.
+        /// </summary>
         private readonly IUniverseMarketCacheFactory marketCacheFactory;
 
+        /// <summary>
+        /// The market data cache strategy factory.
+        /// </summary>
         private readonly IMarketDataCacheStrategyFactory marketDataCacheStrategyFactory;
 
+        /// <summary>
+        /// The cost calculator factory.
+        /// </summary>
         private readonly ICostCalculatorFactory costCalculatorFactory;
 
+        /// <summary>
+        /// The revenue calculator factory.
+        /// </summary>
         private readonly IRevenueCalculatorFactory revenueCalculatorFactory;
 
+        /// <summary>
+        /// The exchange rate profit calculator.
+        /// </summary>
         private readonly IExchangeRateProfitCalculator exchangeRateProfitCalculator;
-        
+
+        /// <summary>
+        /// The stack logger.
+        /// </summary>
         private readonly ILogger<TradingHistoryStack> stackLogger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FixedIncomeHighProfitFactory"/> class.
+        /// </summary>
+        /// <param name="fixedIncomeOrderFilterService">
+        /// The fixed income order filter service.
+        /// </param>
+        /// <param name="marketCacheFactory">
+        /// The market cache factory.
+        /// </param>
+        /// <param name="marketDataCacheStrategyFactory">
+        /// The market data cache strategy factory.
+        /// </param>
+        /// <param name="costCalculatorFactory">
+        /// The cost calculator factory.
+        /// </param>
+        /// <param name="revenueCalculatorFactory">
+        /// The revenue calculator factory.
+        /// </param>
+        /// <param name="exchangeRateProfitCalculator">
+        /// The exchange rate profit calculator.
+        /// </param>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        /// <param name="stackLogger">
+        /// The stack logger.
+        /// </param>
         public FixedIncomeHighProfitFactory(
             IUniverseFixedIncomeOrderFilterService fixedIncomeOrderFilterService,
             IUniverseMarketCacheFactory marketCacheFactory,
@@ -49,9 +103,8 @@
             ILogger<FixedIncomeHighProfitsRule> logger,
             ILogger<TradingHistoryStack> stackLogger)
         {
-            this.fixedIncomeOrderFilterService = fixedIncomeOrderFilterService
-                                                  ?? throw new ArgumentNullException(
-                                                      nameof(fixedIncomeOrderFilterService));
+            this.fixedIncomeOrderFilterService =
+                fixedIncomeOrderFilterService ?? throw new ArgumentNullException(nameof(fixedIncomeOrderFilterService));
             this.marketCacheFactory =
                 marketCacheFactory ?? throw new ArgumentNullException(nameof(marketCacheFactory));
             this.marketDataCacheStrategyFactory = marketDataCacheStrategyFactory ?? throw new ArgumentNullException(nameof(marketDataCacheStrategyFactory));
@@ -63,8 +116,35 @@
             this.stackLogger = stackLogger ?? throw new ArgumentNullException(nameof(stackLogger));
         }
 
+        /// <summary>
+        /// The version of the rule.
+        /// </summary>
         public static string Version => Versioner.Version(1, 0);
 
+        /// <summary>
+        /// The build rule.
+        /// </summary>
+        /// <param name="parameters">
+        /// The parameters.
+        /// </param>
+        /// <param name="ruleContext">
+        /// The rule context.
+        /// </param>
+        /// <param name="judgementService">
+        /// The judgement service.
+        /// </param>
+        /// <param name="dataRequestSubscriber">
+        /// The data request subscriber.
+        /// </param>
+        /// <param name="runMode">
+        /// The run mode.
+        /// </param>
+        /// <param name="scheduledExecution">
+        /// The scheduled execution.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IFixedIncomeHighProfitsRule"/>.
+        /// </returns>
         public IFixedIncomeHighProfitsRule BuildRule(
             IHighProfitsRuleFixedIncomeParameters parameters,
             ISystemProcessOperationRunRuleContext ruleContext,
@@ -79,7 +159,7 @@
                 this.costCalculatorFactory,
                 this.revenueCalculatorFactory,
                 this.exchangeRateProfitCalculator,
-       this.fixedIncomeOrderFilterService,
+                this.fixedIncomeOrderFilterService,
                 this.marketCacheFactory,
                 this.marketDataCacheStrategyFactory,
                 dataRequestSubscriber,
--- src/Surveillance.Engine/Rules/FixedIncome/HighProfits/FixedIncomeHighProfitJudgementMapper.cs
+++ src/Surveillance.Engine/Rules/FixedIncome/HighProfits/FixedIncomeHighProfitJudgementMapper.cs
@@ -8,20 +8,41 @@
     using Surveillance.Engine.Rules.Rules.Equity.HighProfits.Interfaces;
     using Surveillance.Engine.Rules.Rules.Interfaces;
 
+    /// <summary>
+    /// The fixed income high profit judgement mapper.
+    /// </summary>
     public class FixedIncomeHighProfitJudgementMapper : IFixedIncomeHighProfitJudgementMapper
     {
-        private readonly ILogger<FixedIncomeHighProfitJudgementMapper> _logger;
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger<FixedIncomeHighProfitJudgementMapper> logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FixedIncomeHighProfitJudgementMapper"/> class.
+        /// </summary>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
         public FixedIncomeHighProfitJudgementMapper(ILogger<FixedIncomeHighProfitJudgementMapper> logger)
         {
-            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
+        /// <summary>
+        /// The map.
+        /// </summary>
+        /// <param name="judgementContext">
+        /// The judgement context.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IRuleBreach"/>.
+        /// </returns>
         public IRuleBreach Map(IFixedIncomeHighProfitJudgementContext judgementContext)
         {
             if (judgementContext == null)
             {
-                this._logger?.LogInformation($"{nameof(judgementContext)} was null in map. Returning.");
+                this.logger?.LogInformation($"{nameof(judgementContext)} was null in map. Returning.");
                 return null;
             }
 
@@ -33,6 +54,15 @@
             return ruleBreach;
         }
 
+        /// <summary>
+        /// The build description.
+        /// </summary>
+        /// <param name="judgementContext">
+        /// The judgement context.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
         private string BuildDescription(IFixedIncomeHighProfitJudgementContext judgementContext)
         {
             var highRelativeProfitAsPercentage = Math.Round(
@@ -62,6 +92,18 @@
                 $"High profit rule breach detected for {judgementContext.RuleBreachContext.Security.Name} at {judgementContext.RuleBreachContext.UniverseDateTime}.{highRelativeProfitSection}{highAbsoluteProfitSection}{highProfitExchangeRatesSection}";
         }
 
+        /// <summary>
+        /// The high absolute profit text.
+        /// </summary>
+        /// <param name="judgementContext">
+        /// The judgement context.
+        /// </param>
+        /// <param name="absoluteProfit">
+        /// The absolute profit.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
         private string HighAbsoluteProfitText(IFixedIncomeHighProfitJudgementContext judgementContext, decimal absoluteProfit)
         {
             return judgementContext.HasAbsoluteProfitBreach
@@ -69,17 +111,29 @@
                        : string.Empty;
         }
 
+        /// <summary>
+        /// The high profit exchange rate text.
+        /// </summary>
+        /// <param name="judgementContext">
+        /// The judgement context.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
         private string HighProfitExchangeRateText(IFixedIncomeHighProfitJudgementContext judgementContext)
         {
             if (!judgementContext.FixedIncomeParameters.UseCurrencyConversions
-                || judgementContext.ExchangeRateProfits == null) return string.Empty;
+                || judgementContext.ExchangeRateProfits == null)
+            {
+                return string.Empty;
+            }
 
             if (string.Equals(
                 judgementContext.ExchangeRateProfits.FixedCurrency.Code,
                 judgementContext.ExchangeRateProfits.VariableCurrency.Code,
                 StringComparison.InvariantCultureIgnoreCase))
             {
-                this._logger.LogError(
+                this.logger.LogError(
                     $"Fixed Income High Profits had two equal currencies when generating WER text {judgementContext.ExchangeRateProfits.FixedCurrency.Code} and {judgementContext.ExchangeRateProfits.VariableCurrency.Code}");
                 return string.Empty;
             }
@@ -104,12 +158,30 @@
                 2,
                 MidpointRounding.AwayFromZero);
 
-            if (revenueWer == 0 || costWer == 0) return string.Empty;
+            if (revenueWer == 0 || costWer == 0)
+            {
+                return string.Empty;
+            }
 
             return
                 $" The position was acquired with a currency conversion between ({judgementContext.ExchangeRateProfits.FixedCurrency.Code}/{judgementContext.ExchangeRateProfits.VariableCurrency.Code}) rate at a weighted exchange rate of {costWer} and sold at a weighted exchange rate of {revenueWer}. The impact on profits from exchange rate movements was {relativePercentage}% and the absolute amount of profits due to exchange rates is ({judgementContext.AbsoluteProfitCurrency}) {absAmount}.";
         }
 
+        /// <summary>
+        /// The high relative profit text.
+        /// </summary>
+        /// <param name="judgementContext">
+        /// The judgement context.
+        /// </param>
+        /// <param name="highRelativeProfitAsPercentage">
+        /// The high relative profit as percentage.
+        /// </param>
+        /// <param name="highRelativeProfitAsPercentageSetByUser">
+        /// The high relative profit as percentage set by user.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
         private string HighRelativeProfitText(
             IFixedIncomeHighProfitJudgementContext judgementContext,
             decimal highRelativeProfitAsPercentage,

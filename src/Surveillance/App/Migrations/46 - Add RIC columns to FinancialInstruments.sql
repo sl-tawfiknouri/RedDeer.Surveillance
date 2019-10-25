d059969c433d3e062c4bb9400f0972db59e173c4
Jezz Goodwin <jezzgoodwin@gmail.com>
2019-10-25 14:25:40 +0100

    RDPB-4685 add sql migration file

---
+++ src/Surveillance/App/Migrations/46 - Add RIC columns to FinancialInstruments.sql
@@ -0,0 +1,12 @@
+﻿-- ADD RIC TO FINANCIALINSTRUMENTS --
+-- AUTHOR : JEZZ GOODWIN
+-- DATE : 21/10/2019
+-- PURPOSE : Add RIC columns for instrument identification
+START TRANSACTION;
+
+INSERT INTO Migrations VALUES(46, "Add Ric columns to FinancialInstruments.sql", now());
+
+ALTER TABLE FinancialInstruments ADD Ric VARCHAR(30) AFTER BloombergTicker;
+ALTER TABLE FinancialInstruments ADD UnderlyingRic VARCHAR(30) AFTER UnderlyingBloombergTicker;
+
+COMMIT;

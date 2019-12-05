c48698fb2cb46c7f82cb364b305c34395a62213d
reddeer-bencameron <ben.cameron@reddeer.com>
2019-12-05 17:23:56 +0000

    WIP

---
+++ src/IntegrationTests/Runner/FixedIncomeClosePriceMock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Firefly.Service.Data.TickPriceHistory.Shared.Protos;
+using Grpc.Core;
+
+namespace RedDeer.Surveillance.IntegrationTests.Runner
+{
+    public class FixedIncomeClosePriceMock
+    {
+        private readonly List<SecurityTimeBarQuerySubResponse> _securityTimeBarQuerySubResponse = new List<SecurityTimeBarQuerySubResponse>();
+
+        public void Add(SecurityTimeBarQuerySubResponse item)
+        {
+            _securityTimeBarQuerySubResponse.Add(item);
+        }
+
+        public AsyncUnaryCall<GetEodPricingResponse> GetEodPricingAsync()
+        {
+            var response = new GetEodPricingResponse {Success = true};
+            return new AsyncUnaryCall<GetEodPricingResponse>(Task.FromResult(response), null, null, null, null);
+        }
+
+        public AsyncUnaryCall<SecurityTimeBarQueryResponse> QuerySecurityTimeBarsAsync()
+        {
+            var response = new SecurityTimeBarQueryResponse
+            {
+                Success = true
+            };
+            response.SubResponses.AddRange(_securityTimeBarQuerySubResponse);
+
+            return new AsyncUnaryCall<SecurityTimeBarQueryResponse>(Task.FromResult(response), null, null, null, null);
+        }
+    }
+}
